2ebbbaaac631cabf9799777c41f4bcf2483c02d2
yongjoo <ffffgggggffff@gmail.com>
2019-04-04 21:25:09 +0900

    IOS validator detail go on

---
+++ Cosmos-IOS/Cosmostation/Cosmostation/Cell/ValidatorDetailHistoryEmpty.swift
@@ -0,0 +1,24 @@
+//
+//  ValidatorDetailHistoryEmpty.swift
+//  Cosmostation
+//
+//  Created by yongjoo on 04/04/2019.
+//  Copyright © 2019 wannabit. All rights reserved.
+//
+
+import UIKit
+
+class ValidatorDetailHistoryEmpty: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Cosmos-IOS/Cosmostation/Cosmostation/Cell/ValidatorDetailHistoryHeader.swift
@@ -0,0 +1,21 @@
+//
+//  ValidatorDetailHistoryHeader.swift
+//  Cosmostation
+//
+//  Created by yongjoo on 04/04/2019.
+//  Copyright © 2019 wannabit. All rights reserved.
+//
+
+import UIKit
+
+class ValidatorDetailHistoryHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
