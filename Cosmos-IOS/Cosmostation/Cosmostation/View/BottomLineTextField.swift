52bff38eca4c8c57ba4dd2c761e9587cb17096ba
yongjoo <yongjoo@wannabit.io>
2019-12-11 15:11:15 +0900

    IOS - remove border textfiled

--- Cosmos-IOS/Cosmostation/Cosmostation/View/BottomLineTextField.swift
+++ Cosmos-IOS/Cosmostation/Cosmostation/View/BottomLineTextField.swift
@@ -10,45 +10,13 @@ import UIKit
 @IBDesignable
 class BottomLineTextField: UITextField {
     
-//    override var tintColor: UIColor! {
-//
-//        didSet {
-//            setNeedsDisplay()
-//        }
-//    }
-//
-//    override func draw(_ rect: CGRect) {
-//
-//        let startingPoint   = CGPoint(x: rect.minX, y: rect.maxY)
-//        let endingPoint     = CGPoint(x: rect.maxX, y: rect.maxY)
-//
-//        let path = UIBezierPath()
-//
-//        path.move(to: startingPoint)
-//        path.addLine(to: endingPoint)
-//        path.lineWidth = 2.0
-//
-//        if self.isFocused {
-//            UIColor.gray.setStroke()
-//        } else {
-//            UIColor.clear.setStroke()
-//        }
-//
-//
-//
-//        path.stroke()
-//    }
     var bottomBorder = UIView()
     
     override func awakeFromNib() {
-        
-        // Setup Bottom-Border
-//        self.layer.backgroundColor = UIColor.red.cgColor
-        
+        self.borderStyle = .none
         self.translatesAutoresizingMaskIntoConstraints = false
         
         bottomBorder = UIView.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-//        bottomBorder.backgroundColor = UIColor(hexString: "#7A7F88")
         bottomBorder.backgroundColor = UIColor.clear
         bottomBorder.translatesAutoresizingMaskIntoConstraints = false
         
