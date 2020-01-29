ebfe7e949864d2d6825d3b84c6048a61d26808ea
wannabit-yongjoo <yongjoo@wannabit.io>
2020-01-29 12:11:57 +0900

    Android - Ignore file

--- Cosmos-Android/lib/src/main/kotlin/org/walletconnect/Utils.kt
+++
@@ -1,8 +0,0 @@
-package org.walletconnect
-
-//Map functions that throw exceptions into optional types
-fun <T> nullOnThrow(func: () -> T): T? = try {
-    func.invoke()
-} catch (e: Exception) {
-    null
-}
