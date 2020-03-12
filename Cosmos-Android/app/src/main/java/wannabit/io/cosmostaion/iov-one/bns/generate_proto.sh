6496ed3455feb028c825b67099fdfdec9f48600f
orkunkl <kulceorkun@gmail.com>
2020-03-12 22:24:56 +0300

    Exclude docker on IOV android generate proto

--- Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/iov-one/bns/generate_proto.sh
+++ Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/iov-one/bns/generate_proto.sh
@@ -1,11 +1,9 @@
 BNSD_VERSION=1.0.0
 
-rm -rf spec gen
-
+rm -rf gen
 wget https://github.com/iov-one/weave/releases/download/v${BNSD_VERSION}/weave-v${BNSD_VERSION}-proto-spec.tar.gz -O weave-proto-spec.tar.gz
 tar xzvf weave-proto-spec.tar.gz
 rm -rf weave-proto-spec.tar.gz
 
-docker run --rm -v $(pwd):/work iov1/prototool:0.2.2 prototool generate
-
+prototool generate 
 
