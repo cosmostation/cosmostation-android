51b10afa1672a58850a5dfe173d439ed0cbffc39
orkunkl <kulceorkun@gmail.com>
2020-03-12 22:12:05 +0300

    Add generate_proto.sh to IOV/bns

--- Cosmos-IOS/Cosmostation/Cosmostation/IOV/bns/generate_proto.sh
+++ Cosmos-IOS/Cosmostation/Cosmostation/IOV/bns/generate_proto.sh
@@ -1,2 +1,13 @@
-# TODO download weave spec with version
-eval $(prototool generate --dry-run | sed 's/java_out/swift_out/')
+BNSD_VERSION=1.0.0
+
+rm -rf gen/swift
+wget https://github.com/iov-one/weave/releases/download/v${BNSD_VERSION}/weave-v${BNSD_VERSION}-proto-spec.tar.gz -O weave-proto-spec.tar.gz
+tar xzvf weave-proto-spec.tar.gz
+rm -rf weave-proto-spec.tar.gz
+
+mkdir -p gen/swift
+
+OUTPUT=`prototool generate --dry-run | sed 's/--java_out/--swift_out/'`
+
+eval "$OUTPUT"
+
