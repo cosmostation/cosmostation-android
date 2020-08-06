4ed8bf4c9aa04ecc314db49dcec5933c5d15175f
wannabit-yongjoo <yongjoo@wannabit.io>
2020-08-06 14:28:44 +0900

    Android - Memo warn and Qr scan for memo input for send

--- Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/fragment/SendStep2Fragment.java
+++ Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/fragment/SendStep2Fragment.java
@@ -1,28 +1,42 @@
 package wannabit.io.cosmostaion.fragment;
 
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Rect;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 import wannabit.io.cosmostaion.R;
 import wannabit.io.cosmostaion.activities.SendActivity;
-import wannabit.io.cosmostaion.base.BaseChain;
 import wannabit.io.cosmostaion.base.BaseFragment;
 import wannabit.io.cosmostaion.utils.WUtil;
 
 public class SendStep2Fragment extends BaseFragment implements View.OnClickListener {
 
-    private EditText    mMemo;
-    private TextView    mMemoCnt;
-    private Button      mBeforeBtn, mNextBtn;
+    private EditText        mMemo;
+    private TextView        mMemoCnt;
+    private LinearLayout    mMemoWranLayer;
+    private Button          mBeforeBtn, mNextBtn;
+    private LinearLayout    mBtnQr, mBtnPaste, mBtnHistory;
 
     public static SendStep2Fragment newInstance(Bundle bundle) {
         SendStep2Fragment fragment = new SendStep2Fragment();
@@ -40,10 +54,20 @@ public class SendStep2Fragment extends BaseFragment implements View.OnClickListe
         View rootView   = inflater.inflate(R.layout.fragment_tx_step_memo, container, false);
         mMemo           = rootView.findViewById(R.id.et_memo);
         mMemoCnt        = rootView.findViewById(R.id.tv_memoCnt);
+        mMemoWranLayer  = rootView.findViewById(R.id.memo_warn_layer);
         mBeforeBtn      = rootView.findViewById(R.id.btn_before);
         mNextBtn        = rootView.findViewById(R.id.btn_next);
+
+        mBtnQr = rootView.findViewById(R.id.btn_qr);
+        mBtnPaste = rootView.findViewById(R.id.btn_paste);
+        mBtnHistory = rootView.findViewById(R.id.btn_history);
+        mBtnHistory.setVisibility(View.GONE);
+
         mBeforeBtn.setOnClickListener(this);
         mNextBtn.setOnClickListener(this);
+        mBtnQr.setOnClickListener(this);
+        mBtnPaste.setOnClickListener(this);
+        mBtnHistory.setOnClickListener(this);
 
         mMemoCnt.setText("0" + "/" + WUtil.getMaxMemoSize(getSActivity().mBaseChain) + " byte");
 
@@ -78,6 +102,38 @@ public class SendStep2Fragment extends BaseFragment implements View.OnClickListe
                 mMemoCnt.setText("" + WUtil.getCharSize(memo) + "/" + WUtil.getMaxMemoSize(getSActivity().mBaseChain) + " byte");
             }
         });
+
+
+        rootView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            private boolean alreadyOpen;
+            private final int defaultKeyboardHeightDP = 100;
+            private final int EstimatedKeyboardDP = defaultKeyboardHeightDP + (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ? 48 : 0);
+            private final Rect rect = new Rect();
+
+            @Override
+            public void onGlobalLayout() {
+                int estimatedKeyboardHeight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, EstimatedKeyboardDP, rootView.getResources().getDisplayMetrics());
+                rootView.getWindowVisibleDisplayFrame(rect);
+                int heightDiff = rootView.getRootView().getHeight() - (rect.bottom - rect.top);
+                boolean isShown = heightDiff >= estimatedKeyboardHeight;
+                if (isShown == alreadyOpen) {
+                    return;
+                }
+                alreadyOpen = isShown;
+                if (alreadyOpen) {
+                    mMemoWranLayer.setVisibility(View.GONE);
+                } else {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            mMemoWranLayer.setVisibility(View.VISIBLE);
+                        }
+                    },100);
+                }
+            }
+        });
+        mMemoWranLayer.setVisibility(View.VISIBLE);
+
         return rootView;
     }
 
@@ -94,7 +150,25 @@ public class SendStep2Fragment extends BaseFragment implements View.OnClickListe
             } else {
                 Toast.makeText(getContext(), R.string.error_invalid_memo, Toast.LENGTH_SHORT).show();
             }
+        } else if (v.equals(mBtnQr)) {
+            IntentIntegrator integrator = IntentIntegrator.forSupportFragment(this);
+            integrator.setOrientationLocked(true);
+            integrator.initiateScan();
 
+        } else if (v.equals(mBtnPaste)) {
+            ClipboardManager clipboard = (ClipboardManager)getSActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+            if(clipboard.getPrimaryClip() != null && clipboard.getPrimaryClip().getItemCount() > 0) {
+                String userPaste = clipboard.getPrimaryClip().getItemAt(0).coerceToText(getSActivity()).toString().trim();
+                if(TextUtils.isEmpty(userPaste)) {
+                    Toast.makeText(getSActivity(), R.string.error_clipboard_no_data, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mMemo.setText(userPaste);
+                mMemo.setSelection(mMemo.getText().length());
+
+            } else {
+                Toast.makeText(getSActivity(), R.string.error_clipboard_no_data, Toast.LENGTH_SHORT).show();
+            }
 
         }
 
@@ -102,5 +176,18 @@ public class SendStep2Fragment extends BaseFragment implements View.OnClickListe
 
     private SendActivity getSActivity() {
         return (SendActivity)getBaseActivity();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() != null) {
+                mMemo.setText(result.getContents().trim());
+                mMemo.setSelection(mMemo.getText().length());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
