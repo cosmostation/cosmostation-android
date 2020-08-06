ace14bd6c64d09916dcd4bc55c076adecc133172
wannabit-yongjoo <yongjoo@wannabit.io>
2020-08-06 14:56:41 +0900

    Android - Adding watching address support Qr code scan & paste

--- Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/activities/WatchingAccountAddActivity.java
+++ Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/activities/WatchingAccountAddActivity.java
@@ -1,12 +1,21 @@
 package wannabit.io.cosmostaion.activities;
 
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import wannabit.io.cosmostaion.R;
 import wannabit.io.cosmostaion.base.BaseActivity;
@@ -25,6 +34,7 @@ public class WatchingAccountAddActivity extends BaseActivity implements View.OnC
     private Toolbar mToolbar;
     private EditText mInput;
     private Button mCancel, mNext;
+    private LinearLayout mBtnQr, mBtnPaste, mBtnHistory;
 
     private String mUserInput;
 
@@ -37,6 +47,10 @@ public class WatchingAccountAddActivity extends BaseActivity implements View.OnC
         mInput          = findViewById(R.id.et_address);
         mCancel         = findViewById(R.id.btn_cancel);
         mNext           = findViewById(R.id.btn_next);
+        mBtnQr          = findViewById(R.id.btn_qr);
+        mBtnPaste       = findViewById(R.id.btn_paste);
+        mBtnHistory     = findViewById(R.id.btn_history);
+        mBtnHistory.setVisibility(View.GONE);
 
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
@@ -44,6 +58,9 @@ public class WatchingAccountAddActivity extends BaseActivity implements View.OnC
 
         mCancel.setOnClickListener(this);
         mNext.setOnClickListener(this);
+        mBtnQr.setOnClickListener(this);
+        mBtnPaste.setOnClickListener(this);
+        mBtnHistory.setOnClickListener(this);
     }
 
     @Override
@@ -160,6 +177,27 @@ public class WatchingAccountAddActivity extends BaseActivity implements View.OnC
                 Toast.makeText(getBaseContext(), R.string.error_invalid_address, Toast.LENGTH_SHORT).show();
                 return;
             }
+
+        } else if (v.equals(mBtnQr)) {
+            IntentIntegrator integrator = new IntentIntegrator(this);
+            integrator.setOrientationLocked(true);
+            integrator.initiateScan();
+
+        } else if (v.equals(mBtnPaste)) {
+            ClipboardManager clipboard = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
+            if(clipboard.getPrimaryClip() != null && clipboard.getPrimaryClip().getItemCount() > 0) {
+                String userPaste = clipboard.getPrimaryClip().getItemAt(0).coerceToText(this).toString().trim();
+                if(TextUtils.isEmpty(userPaste)) {
+                    Toast.makeText(this, R.string.error_clipboard_no_data, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mInput.setText(userPaste);
+                mInput.setSelection(mInput.getText().length());
+
+            } else {
+                Toast.makeText(this, R.string.error_clipboard_no_data, Toast.LENGTH_SHORT).show();
+            }
+
         }
 
     }
@@ -191,6 +229,19 @@ public class WatchingAccountAddActivity extends BaseActivity implements View.OnC
                 }
 
             }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() != null) {
+                mInput.setText(result.getContents().trim());
+                mInput.setSelection(mInput.getText().length());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
         }
     }
 }
