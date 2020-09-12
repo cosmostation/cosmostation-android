811012a22a305114905a645bdbfa10aff79ed5b0
wannabit-yongjoo <yongjoo@wannabit.io>
2020-09-12 13:41:27 +0900

    Android - update BEP3 model for kava-t-9000

---
+++ Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/network/res/ResKavaBep3Param2.java
@@ -0,0 +1,99 @@
+package wannabit.io.cosmostaion.network.res;
+
+import com.google.gson.annotations.SerializedName;
+
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+import wannabit.io.cosmostaion.utils.WLog;
+
+public class ResKavaBep3Param2 {
+    @SerializedName("height")
+    public String height;
+
+    @SerializedName("result")
+    public KavaBep3AssetParams result;
+
+
+    public class KavaBep3AssetParams {
+        @SerializedName("asset_params")
+        public ArrayList<KavaBep3AssetParam> asset_params;
+    }
+
+    public class KavaBep3AssetParam {
+        @SerializedName("denom")
+        public String denom;
+
+        @SerializedName("coin_id")
+        public String coin_id;
+
+        @SerializedName("active")
+        public boolean active;
+
+        @SerializedName("deputy_address")
+        public String deputy_address;
+
+        @SerializedName("fixed_fee")
+        public String fixed_fee;
+
+        @SerializedName("min_swap_amount")
+        public String min_swap_amount;
+
+        @SerializedName("max_swap_amount")
+        public String max_swap_amount;
+
+        @SerializedName("min_block_lock")
+        public String min_block_lock;
+
+        @SerializedName("max_block_lock")
+        public String max_block_lock;
+
+        @SerializedName("supply_limit")
+        public KavaBep3SupplyLimit supply_limit;
+
+    }
+
+    public class KavaBep3SupplyLimit {
+        @SerializedName("limit")
+        public String limit;
+
+        @SerializedName("time_limited")
+        public boolean time_limited;
+
+        @SerializedName("time_period")
+        public String time_period;
+
+        @SerializedName("time_based_limit")
+        public String time_based_limit;
+
+    }
+
+
+    public KavaBep3AssetParam getSupportedSwapAsset(String denom) {
+        for (KavaBep3AssetParam asset:result.asset_params) {
+            if (denom.toLowerCase().startsWith(asset.denom.toLowerCase())) {
+                return asset;
+            }
+        }
+        return null;
+    }
+
+    public BigDecimal getSupportedSwapAssetLimit(String denom) {
+        BigDecimal limit = BigDecimal.ZERO;
+        try {
+            limit = new BigDecimal(getSupportedSwapAsset(denom).supply_limit.limit);
+        } catch (Exception e) {}
+        return limit;
+    }
+
+    public BigDecimal getSupportedSwapAssetMaxOnce(String denom) {
+        BigDecimal once = BigDecimal.ZERO;
+        try {
+            once = new BigDecimal(getSupportedSwapAsset(denom).max_swap_amount);
+        } catch (Exception e) {}
+        return once;
+    }
+
+}
---
+++ Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/network/res/ResKavaSwapSupply2.java
@@ -0,0 +1,62 @@
+package wannabit.io.cosmostaion.network.res;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+import wannabit.io.cosmostaion.model.type.Coin;
+import wannabit.io.cosmostaion.utils.WLog;
+
+public class ResKavaSwapSupply2 {
+
+    @SerializedName("height")
+    public String height;
+
+    @SerializedName("result")
+    public ArrayList<KavaSwapSupply2> result;
+
+
+    public class KavaSwapSupply2{
+        @SerializedName("incoming_supply")
+        public Coin incoming_supply;
+
+        @SerializedName("outgoing_supply")
+        public Coin outgoing_supply;
+
+        @SerializedName("current_supply")
+        public Coin current_supply;
+
+        @SerializedName("time_limited_current_supply")
+        public Coin time_limited_current_supply;
+
+        @SerializedName("time_elapsed")
+        public String time_elapsed;
+
+    }
+
+
+    public KavaSwapSupply2 getSwapSupply(String denom) {
+        if (result != null && result.size() > 0) {
+            for (KavaSwapSupply2 supply:result) {
+                if (denom.toLowerCase().startsWith(supply.incoming_supply.denom.toLowerCase())) {
+                    return supply;
+                }
+            }
+        }
+        return null;
+    }
+
+    public BigDecimal getRemainCap(String denom, BigDecimal supplyLimit) {
+        BigDecimal remain = BigDecimal.ZERO;
+        try {
+            KavaSwapSupply2 supply = getSwapSupply(denom);
+            BigDecimal incoming_supply = new BigDecimal(supply.incoming_supply.amount);
+            BigDecimal current_supply = new BigDecimal(supply.current_supply.amount);
+            remain = supplyLimit.subtract(current_supply).subtract(incoming_supply);
+
+        }catch (Exception e) {}
+        return remain;
+    }
+
+}
