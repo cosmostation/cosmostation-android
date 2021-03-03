be1e955eea5bc249ac448fbf47f473f9998d9402
wannabit-yongjoo <yongjoo@wannabit.io>
2021-03-03 16:57:17 +0900

    Android - Hide acution

--- Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/fragment/chains/kava/ListAuctionFragment.java
+++ Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/fragment/chains/kava/ListAuctionFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,7 +18,8 @@ public class ListAuctionFragment extends BaseFragment {
 
     private SwipeRefreshLayout  mSwipeRefreshLayout;
     private RecyclerView        mRecyclerView;
-    private RelativeLayout  mProgress;
+    private RelativeLayout      mProgress;
+    private TextView            mNotYet;
 
     public static ListAuctionFragment newInstance(Bundle bundle) {
         ListAuctionFragment fragment = new ListAuctionFragment();
@@ -32,7 +34,20 @@ public class ListAuctionFragment extends BaseFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_cdp_market, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_auctions_list, container, false);
+        mSwipeRefreshLayout     = rootView.findViewById(R.id.layer_refresher);
+        mRecyclerView           = rootView.findViewById(R.id.recycler);
+        mProgress               = rootView.findViewById(R.id.reward_progress);
+        mNotYet                 = rootView.findViewById(R.id.text_not_yet);
         return rootView;
+    }
+
+    @Override
+    public void onRefreshTab() {
+        mSwipeRefreshLayout.setVisibility(View.GONE);
+        mRecyclerView.setVisibility(View.GONE);
+        mProgress.setVisibility(View.GONE);
+        mNotYet.setVisibility(View.VISIBLE);
+
     }
 }
