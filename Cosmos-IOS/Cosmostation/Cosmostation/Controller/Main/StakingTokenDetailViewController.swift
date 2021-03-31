51bb8992197478d05a4f6771f8f8715b6554602e
wannabit-yongjoo <yongjoo@wannabit.io>
2021-03-31 11:42:11 +0900

    iOS - send btn icon update

--- Cosmos-IOS/Cosmostation/Cosmostation/Controller/Main/StakingTokenDetailViewController.swift
+++ Cosmos-IOS/Cosmostation/Cosmostation/Controller/Main/StakingTokenDetailViewController.swift
@@ -72,6 +72,30 @@ class StakingTokenDetailViewController: BaseViewController, UITableViewDelegate,
         self.shareAddress(account!.account_address, nickName!)
     }
     
+    @IBAction func onClickSend(_ sender: UIButton) {
+        if (account!.account_has_private == false) {
+            self.onShowAddMenomicDialog()
+            return
+        }
+        
+        let txVC = UIStoryboard(name: "GenTx", bundle: nil).instantiateViewController(withIdentifier: "TransactionViewController") as! TransactionViewController
+        if (WUtils.isGRPC(chainType!)) {
+            let feeAmount = WUtils.getEstimateGasFeeAmount(chainType!, COSMOS_MSG_TYPE_TRANSFER2, 0)
+            if (BaseData.instance.getAvailableAmount(WUtils.getMainDenom(chainType)).compare(feeAmount).rawValue <= 0) {
+                self.onShowToast(NSLocalizedString("error_not_enough_balance_to_send", comment: ""))
+                return
+            }
+            txVC.mToSendDenom = WUtils.getMainDenom(chainType)
+            txVC.mType = COSMOS_MSG_TYPE_TRANSFER2
+            
+        } else {
+            return
+            
+        }
+        txVC.hidesBottomBarWhenPushed = true
+        self.navigationItem.title = ""
+        self.navigationController?.pushViewController(txVC, animated: true)
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if (hasVesting == true) {
