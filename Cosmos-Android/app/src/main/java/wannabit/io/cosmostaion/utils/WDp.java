865ad885013e7ec02dcebff0c829a58aa550e032
wannabit-yongjoo <yongjoo@wannabit.io>
2021-04-17 15:05:32 +0900

    Android - Reward address change & vote for Cryto.org

--- Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/utils/WDp.java
+++ Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/utils/WDp.java
@@ -1349,7 +1349,7 @@ public class WDp {
                 result = unbondFormat.format(calendar.getTimeInMillis());
                 return result + "   " +c.getString(R.string.str_unbonding_14days_after);
 
-            } else if (chain.equals(SENTINEL_MAIN)) {
+            } else if (chain.equals(SENTINEL_MAIN) || chain.equals(CRYTO_MAIN)) {
                 Calendar calendar = Calendar.getInstance();
                 calendar.add(Calendar.DATE, 28);
                 SimpleDateFormat unbondFormat = new SimpleDateFormat(c.getString(R.string.str_dp_time_format2));
