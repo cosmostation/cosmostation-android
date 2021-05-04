537f32307d1e290491ed2d143da8c98b7d329d80
Kwonhyukjoon <victorehko12@naver.com>
2021-05-04 14:02:51 +0900

    Android - get reward, reinvest for Kichain

--- Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/fragment/ReInvestStep2Fragment.java
+++ Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/fragment/ReInvestStep2Fragment.java
@@ -38,6 +38,7 @@ import static wannabit.io.cosmostaion.base.BaseChain.IOV_MAIN;
 import static wannabit.io.cosmostaion.base.BaseChain.IOV_TEST;
 import static wannabit.io.cosmostaion.base.BaseChain.KAVA_MAIN;
 import static wannabit.io.cosmostaion.base.BaseChain.KAVA_TEST;
+import static wannabit.io.cosmostaion.base.BaseChain.KI_MAIN;
 import static wannabit.io.cosmostaion.base.BaseChain.SECRET_MAIN;
 import static wannabit.io.cosmostaion.base.BaseChain.SENTINEL_MAIN;
 import static wannabit.io.cosmostaion.base.BaseChain.SIF_MAIN;
@@ -50,6 +51,8 @@ import static wannabit.io.cosmostaion.base.BaseConstant.FEE_IOV_GAS_RATE_AVERAGE
 import static wannabit.io.cosmostaion.base.BaseConstant.FEE_KAVA_GAS_AMOUNT_REINVEST;
 import static wannabit.io.cosmostaion.base.BaseConstant.FETCH_GAS_AMOUNT_REINVEST;
 import static wannabit.io.cosmostaion.base.BaseConstant.FETCH_GAS_FEE_RATE_AVERAGE;
+import static wannabit.io.cosmostaion.base.BaseConstant.KI_GAS_AMOUNT_REINVEST;
+import static wannabit.io.cosmostaion.base.BaseConstant.KI_GAS_FEE_RATE_AVERAGE;
 import static wannabit.io.cosmostaion.base.BaseConstant.SECRET_GAS_AMOUNT_REINVEST;
 import static wannabit.io.cosmostaion.base.BaseConstant.SECRET_GAS_FEE_RATE_AVERAGE;
 import static wannabit.io.cosmostaion.base.BaseConstant.SENTINEL_GAS_AMOUNT_REINVEST;
@@ -293,6 +296,21 @@ public class ReInvestStep2Fragment extends BaseFragment implements View.OnClickL
 
             mGasFeeAmount.setText(WDp.getDpAmount2(getContext(), mFeeAmount, 18, 18));
             mGasFeePrice.setText(WDp.getDpMainAssetValue(getContext(), getBaseDao(), mFeeAmount, getSActivity().mBaseChain));
+
+        } else if (getSActivity().mBaseChain.equals(KI_MAIN)) {
+            mFeeLayer1.setVisibility(View.GONE);
+            mFeeLayer2.setVisibility(View.VISIBLE);
+            mFeeLayer3.setVisibility(View.GONE);
+
+            mSpeedImg.setImageDrawable(getResources().getDrawable(R.drawable.fee_img));
+            mSpeedMsg.setText(getString(R.string.str_fee_speed_title_ki));
+
+            mGasAmount.setText(KI_GAS_AMOUNT_REINVEST);
+            mGasRate.setText(WDp.getDpString(KI_GAS_FEE_RATE_AVERAGE, 2));
+            mFeeAmount = WUtil.getEstimateGasFeeAmount(getContext(), getSActivity().mBaseChain, CONST_PW_TX_REINVEST, 0);
+
+            mGasFeeAmount.setText(WDp.getDpAmount2(getContext(), mFeeAmount, 6, 6));
+            mGasFeePrice.setText(WDp.getDpMainAssetValue(getContext(), getBaseDao(), mFeeAmount, getSActivity().mBaseChain));
         }
         return rootView;
     }
@@ -414,6 +432,17 @@ public class ReInvestStep2Fragment extends BaseFragment implements View.OnClickL
                 amount.add(gasCoin);
                 fee.amount = amount;
                 fee.gas = SIF_GAS_AMOUNT_REINVEST;
+                getSActivity().mTxFee = fee;
+
+            } else if (getSActivity().mBaseChain.equals(KI_MAIN)) {
+                Fee fee = new Fee();
+                Coin gasCoin = new Coin();
+                gasCoin.denom = WDp.mainDenom(getSActivity().mBaseChain);
+                gasCoin.amount = mFeeAmount.toPlainString();
+                ArrayList<Coin> amount = new ArrayList<>();
+                amount.add(gasCoin);
+                fee.amount = amount;
+                fee.gas = KI_GAS_AMOUNT_REINVEST;
                 getSActivity().mTxFee = fee;
 
             }
