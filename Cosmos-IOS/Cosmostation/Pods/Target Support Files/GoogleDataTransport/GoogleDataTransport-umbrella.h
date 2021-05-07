ba7b1b1d55791f078fe979eb619747accf208bd0
wannabit-yongjoo <yongjoo@wannabit.io>
2021-05-07 18:04:43 +0900

    iOS - pod update

--- Cosmos-IOS/Cosmostation/Pods/Target Support Files/GoogleDataTransport/GoogleDataTransport-umbrella.h
+++ Cosmos-IOS/Cosmostation/Pods/Target Support Files/GoogleDataTransport/GoogleDataTransport-umbrella.h
@@ -10,21 +10,14 @@
 #endif
 #endif
 
-#import "GDTCORAssert.h"
 #import "GDTCORClock.h"
 #import "GDTCORConsoleLogger.h"
+#import "GDTCOREndpoints.h"
 #import "GDTCOREvent.h"
 #import "GDTCOREventDataObject.h"
 #import "GDTCOREventTransformer.h"
-#import "GDTCORLifecycle.h"
-#import "GDTCORPlatform.h"
-#import "GDTCORPrioritizer.h"
-#import "GDTCORReachability.h"
-#import "GDTCORRegistrar.h"
 #import "GDTCORTargets.h"
 #import "GDTCORTransport.h"
-#import "GDTCORUploader.h"
-#import "GDTCORUploadPackage.h"
 #import "GoogleDataTransport.h"
 
 FOUNDATION_EXPORT double GoogleDataTransportVersionNumber;
