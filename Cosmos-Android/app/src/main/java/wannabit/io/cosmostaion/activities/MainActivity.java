dd0dc3ae36fa194366c3d08def760783f6fc9656
Kwonhyukjoon <victorehko12@naver.com>
2021-05-17 12:01:41 +0900

    Android - Explorer button error update

--- Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/activities/MainActivity.java
+++ Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/activities/MainActivity.java
@@ -471,7 +471,12 @@ public class MainActivity extends BaseActivity implements FetchCallBack {
     }
 
     public void onExplorerView() {
-        String url  = WUtil.getExplorer(mBaseChain) + "account/" + mAccount.address;
+        String url;
+        if (mBaseChain.equals(SECRET_MAIN)) {
+            url = WUtil.getExplorer(mBaseChain) + "accounts/" + mAccount.address;
+        } else {
+            url  = WUtil.getExplorer(mBaseChain) + "account/" + mAccount.address;
+        }
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         startActivity(intent);
     }
