749ac0e94e6d4a9d3ca6fcd4815eed9735067314
Kwonhyukjoon <victorehko12@naver.com>
2021-05-24 17:14:31 +0900

    Android - import log Delete

--- Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/activities/ValidatorActivity.java
+++ Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/activities/ValidatorActivity.java
@@ -5,7 +5,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
