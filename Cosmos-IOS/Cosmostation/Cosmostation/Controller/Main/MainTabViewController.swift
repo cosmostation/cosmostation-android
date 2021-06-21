314bb927b51c493be4b069da5d4c42c0d2b55afd
wannabit-yongjoo <yongjoo@wannabit.io>
2021-06-21 20:44:57 +0900

    iOS - update firebase

--- Cosmos-IOS/Cosmostation/Cosmostation/Controller/Main/MainTabViewController.swift
+++ Cosmos-IOS/Cosmostation/Cosmostation/Controller/Main/MainTabViewController.swift
@@ -1278,7 +1278,7 @@ class MainTabViewController: UITabBarController, UITabBarControllerDelegate, SBC
                 if let params = res as? NSDictionary {
                     BaseData.instance.mParam = Param.init(params)
                 }
-                print("mParam ", BaseData.instance.mParam)
+//                print("mParam ", BaseData.instance.mParam)
             
             case .failure(let error):
                 if (SHOW_LOG) { print("onFetchPriceInfo ", error) }
