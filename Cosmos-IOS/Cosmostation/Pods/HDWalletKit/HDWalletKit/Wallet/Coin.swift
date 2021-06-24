0de687b2e0d1707396f3bad98d4217907b193234
wannabit-yongjoo <yongjoo@wannabit.io>
2021-06-24 19:23:27 +0900

    iOS - update mody HDWalletKit

---
+++ Cosmos-IOS/Cosmostation/Pods/HDWalletKit/HDWalletKit/Wallet/Coin.swift
@@ -0,0 +1,123 @@
+//
+//  Coin.swift
+//  HDWalletKit
+//
+//  Created by Pavlo Boiko on 10/3/18.
+//  Copyright © 2018 Essentia. All rights reserved.
+//
+
+import Foundation
+
+public enum Coin {
+    case bitcoin
+    case ethereum
+    case litecoin
+    case bitcoinCash
+    case hdac
+    
+    //https://github.com/satoshilabs/slips/blob/master/slip-0132.md
+    public var privateKeyVersion: UInt32 {
+        switch self {
+        case .litecoin:
+            return 0x019D9CFE
+        case .hdac: fallthrough
+        case .bitcoin:
+            return 0x0488ADE4
+        default:
+            return 0x0488ADE4
+        }
+    }
+    
+    public var publicKeyVersion: UInt32 {
+        switch self {
+        case .litecoin:
+            return 0x019DA462
+        case .hdac: fallthrough
+        case .bitcoin:
+            return 0x0488B21E
+        default:
+            return 0x0488B21E
+        }
+    }
+    
+    public var publicKeyHash: UInt8 {
+        switch self {
+        case .litecoin:
+            return 0x30
+        case .bitcoin:
+            return 0x00
+        case .hdac:
+            return 0x28
+        default:
+            return 0x00
+        }
+    }
+    
+    //https://www.reddit.com/r/litecoin/comments/6vc8tc/how_do_i_convert_a_raw_private_key_to_wif_for/
+    public var wifPrefix: UInt8 {
+        switch self {
+        case .litecoin:
+            return 0xB0
+        case .bitcoin:
+            return 0x80
+        case .hdac:
+            return 0x80
+        default:
+            return 0x80
+        }
+    }
+    
+    public var scripthash: UInt8 {
+        switch self {
+        case .litecoin:
+            return 0xB0
+        case .bitcoin:
+            return 0x80
+        default:
+            return 0x80
+        }
+    }
+    
+    public var addressPrefix:String {
+        switch self {
+        case .ethereum:
+            return "0x"
+        default:
+            return ""
+        }
+    }
+    
+    public var uncompressedPkSuffix: UInt8 {
+        return 0x01
+    }
+    
+    
+    public var coinType: UInt32 {
+        switch self {
+        case .bitcoin:
+            return 0
+        case .litecoin:
+            return 2
+        case .ethereum:
+            return 60
+        case .bitcoinCash:
+            return 145
+        case .hdac:
+            return 200
+        }
+    }
+    
+    public var scheme: String {
+        switch self {
+        case .bitcoin:
+            return "bitcoin"
+        case .litecoin:
+            return "litecoin"
+        case .bitcoinCash:
+            return "bitcoincash"
+        case .hdac:
+            return "hdac"
+        default: return ""
+        }
+    }
+}
