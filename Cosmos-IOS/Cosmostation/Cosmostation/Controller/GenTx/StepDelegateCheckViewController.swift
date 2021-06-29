69f87ea64828aa3573206e555ce109531178de50
yongjoo <yongjoo@wannabit.io>
2021-06-29 14:49:00 +0900

    iOS - update starnme displaing unbonding time

--- Cosmos-IOS/Cosmostation/Cosmostation/Controller/GenTx/StepDelegateCheckViewController.swift
+++ Cosmos-IOS/Cosmostation/Cosmostation/Controller/GenTx/StepDelegateCheckViewController.swift
@@ -36,9 +36,7 @@ class StepDelegateCheckViewController: BaseViewController, PasswordViewDelegate,
 
     @IBAction func onClickConfirm(_ sender: Any) {
         let popupVC = DelegateWarnPopup(nibName: "DelegateWarnPopup", bundle: nil)
-        if (pageHolderVC.chainType! == ChainType.IOV_MAIN || pageHolderVC.chainType! == ChainType.IOV_TEST) {
-            popupVC.warnImgType = 3
-        } else if (pageHolderVC.chainType! == ChainType.SENTINEL_MAIN || pageHolderVC.chainType! == ChainType.CRYPTO_MAIN) {
+        if (pageHolderVC.chainType! == ChainType.SENTINEL_MAIN || pageHolderVC.chainType! == ChainType.CRYPTO_MAIN) {
             popupVC.warnImgType = 28
         } else {
             popupVC.warnImgType = 21
