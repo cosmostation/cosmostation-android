0ecfc35ff0f6b17b81666febab00d49c496ad1ce
Kwonhyukjoon <victorehko12@naver.com>
2021-08-24 16:32:48 +0900

    Android - StakingToken Grpc Detail Update

--- Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/activities/tokenDetail/StakingTokenGrpcActivity.java
+++ Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/activities/tokenDetail/StakingTokenGrpcActivity.java
@@ -27,49 +27,13 @@ import wannabit.io.cosmostaion.base.BaseActivity;
 import wannabit.io.cosmostaion.base.BaseChain;
 import wannabit.io.cosmostaion.dialog.Dialog_AccountShow;
 import wannabit.io.cosmostaion.utils.WDp;
-import wannabit.io.cosmostaion.utils.WKey;
 import wannabit.io.cosmostaion.utils.WUtil;
 import wannabit.io.cosmostaion.widget.BaseHolder;
 import wannabit.io.cosmostaion.widget.HistoryHolder;
-import wannabit.io.cosmostaion.widget.tokenDetail.TokenAkashHolder;
-import wannabit.io.cosmostaion.widget.tokenDetail.TokenAltheaHolder;
-import wannabit.io.cosmostaion.widget.tokenDetail.TokenBnbHolder;
-import wannabit.io.cosmostaion.widget.tokenDetail.TokenCosmosHolder;
-import wannabit.io.cosmostaion.widget.tokenDetail.TokenCrytoHolder;
-import wannabit.io.cosmostaion.widget.tokenDetail.TokenIrisHolder;
-import wannabit.io.cosmostaion.widget.tokenDetail.TokenKavaHolder;
-import wannabit.io.cosmostaion.widget.tokenDetail.TokenMediblocHolder;
-import wannabit.io.cosmostaion.widget.tokenDetail.TokenOKExHolder;
-import wannabit.io.cosmostaion.widget.tokenDetail.TokenOsmosisHolder;
-import wannabit.io.cosmostaion.widget.tokenDetail.TokenPersisHolder;
-import wannabit.io.cosmostaion.widget.tokenDetail.TokenRizonHolder;
-import wannabit.io.cosmostaion.widget.tokenDetail.TokenSentinelHolder;
-import wannabit.io.cosmostaion.widget.tokenDetail.TokenSifHolder;
-import wannabit.io.cosmostaion.widget.tokenDetail.TokenStarnameHolder;
+import wannabit.io.cosmostaion.widget.tokenDetail.TokenStakingNewHolder;
+import wannabit.io.cosmostaion.widget.tokenDetail.UnBondingHolder;
 import wannabit.io.cosmostaion.widget.tokenDetail.VestingHolder;
 
-import static wannabit.io.cosmostaion.base.BaseChain.AKASH_MAIN;
-import static wannabit.io.cosmostaion.base.BaseChain.ALTHEA_TEST;
-import static wannabit.io.cosmostaion.base.BaseChain.BNB_MAIN;
-import static wannabit.io.cosmostaion.base.BaseChain.BNB_TEST;
-import static wannabit.io.cosmostaion.base.BaseChain.COSMOS_MAIN;
-import static wannabit.io.cosmostaion.base.BaseChain.COSMOS_TEST;
-import static wannabit.io.cosmostaion.base.BaseChain.CRYPTO_MAIN;
-import static wannabit.io.cosmostaion.base.BaseChain.IOV_MAIN;
-import static wannabit.io.cosmostaion.base.BaseChain.IRIS_MAIN;
-import static wannabit.io.cosmostaion.base.BaseChain.IRIS_TEST;
-import static wannabit.io.cosmostaion.base.BaseChain.KAVA_MAIN;
-import static wannabit.io.cosmostaion.base.BaseChain.KAVA_TEST;
-import static wannabit.io.cosmostaion.base.BaseChain.MEDI_MAIN;
-import static wannabit.io.cosmostaion.base.BaseChain.MEDI_TEST;
-import static wannabit.io.cosmostaion.base.BaseChain.OKEX_MAIN;
-import static wannabit.io.cosmostaion.base.BaseChain.OK_TEST;
-import static wannabit.io.cosmostaion.base.BaseChain.OSMOSIS_MAIN;
-import static wannabit.io.cosmostaion.base.BaseChain.PERSIS_MAIN;
-import static wannabit.io.cosmostaion.base.BaseChain.RIZON_TEST;
-import static wannabit.io.cosmostaion.base.BaseChain.SENTINEL_MAIN;
-import static wannabit.io.cosmostaion.base.BaseChain.SIF_MAIN;
-import static wannabit.io.cosmostaion.base.BaseChain.isGRPC;
 import static wannabit.io.cosmostaion.base.BaseConstant.CONST_PW_TX_SIMPLE_SEND;
 
 public class StakingTokenGrpcActivity extends BaseActivity implements View.OnClickListener {
@@ -131,10 +95,8 @@ public class StakingTokenGrpcActivity extends BaseActivity implements View.OnCli
         mDivideDecimal = WDp.mainDivideDecimal(mBaseChain);
         mDisplayDecimal = WDp.mainDivideDecimal(mBaseChain);
 
-        if (isGRPC(mBaseChain)) {
-            if (getBaseDao().onParseRemainVestingsByDenom(WDp.mainDenom(mBaseChain)).size() > 0) { mHasVesting = true; }
-            mBtnIbcSend.setVisibility(View.VISIBLE);
-        }
+        if (getBaseDao().onParseRemainVestingsByDenom(WDp.mainDenom(mBaseChain)).size() > 0) { mHasVesting = true; }
+        mBtnIbcSend.setVisibility(View.VISIBLE);
 
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         mRecyclerView.setHasFixedSize(true);
@@ -222,60 +184,24 @@ public class StakingTokenGrpcActivity extends BaseActivity implements View.OnCli
         }
     }
 
-
-    private static final int TYPE_ATOM                  = 0;
-    private static final int TYPE_IRIS                  = 1;
-    private static final int TYPE_AKASH                 = 2;
-    private static final int TYPE_SENTINEL              = 3;
-    private static final int TYPE_PERSISTENCE           = 4;
-    private static final int TYPE_CRYPTO                = 5;
-    private static final int TYPE_RIZON                 = 6;
-    private static final int TYPE_ALTHEA                = 7;
-    private static final int TYPE_OSMOSIS               = 8;
-    private static final int TYPE_STARNAME              = 9;
-    private static final int TYPE_SIF                   = 10;
-    private static final int TYPE_MEDI                  = 11;
-//    private static final int TYPE_BAND                  = 10;
-
-    private static final int TYPE_VESTING               = 99;
-    private static final int TYPE_HISTORY               = 100;
-
     private class StakingTokenGrpcAdapter extends RecyclerView.Adapter<BaseHolder> {
+        private static final int TYPE_STAKE_NEW             = 0;
+
+        private static final int TYPE_VESTING               = 98;
+        private static final int TYPE_UNBONDING             = 99;
+        private static final int TYPE_HISTORY               = 100;
 
         @NonNull
         @Override
         public BaseHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
-            if (viewType == TYPE_ATOM) {
-                return new TokenCosmosHolder(getLayoutInflater().inflate(R.layout.layout_card_atom, viewGroup, false));
-            } else if (viewType == TYPE_IRIS) {
-                return new TokenIrisHolder(getLayoutInflater().inflate(R.layout.layout_card_iris, viewGroup, false));
-            } else if (viewType == TYPE_AKASH) {
-                return new TokenAkashHolder(getLayoutInflater().inflate(R.layout.layout_card_akash, viewGroup, false));
-            } else if (viewType == TYPE_SENTINEL) {
-                return new TokenSentinelHolder(getLayoutInflater().inflate(R.layout.layout_card_sentinel, viewGroup, false));
-            } else if (viewType == TYPE_PERSISTENCE) {
-                return new TokenPersisHolder(getLayoutInflater().inflate(R.layout.layout_card_persistence, viewGroup, false));
-            } else if (viewType == TYPE_CRYPTO) {
-                return new TokenCrytoHolder(getLayoutInflater().inflate(R.layout.layout_card_cryto, viewGroup, false));
-            } else if (viewType == TYPE_RIZON) {
-                return new TokenRizonHolder(getLayoutInflater().inflate(R.layout.layout_card_rizon, viewGroup, false));
-            } else if (viewType == TYPE_ALTHEA) {
-                return new TokenAltheaHolder(getLayoutInflater().inflate(R.layout.layout_card_althea, viewGroup, false));
-            } else if (viewType == TYPE_OSMOSIS) {
-                return new TokenOsmosisHolder(getLayoutInflater().inflate(R.layout.layout_card_osmosis, viewGroup, false));
-            } else if (viewType == TYPE_STARNAME) {
-                return new TokenStarnameHolder(getLayoutInflater().inflate(R.layout.layout_card_starname, viewGroup, false));
-            } else if (viewType == TYPE_SIF) {
-                return new TokenSifHolder(getLayoutInflater().inflate(R.layout.layout_card_sif, viewGroup, false));
-            } else if (viewType == TYPE_MEDI) {
-                return new TokenMediblocHolder(getLayoutInflater().inflate(R.layout.layout_card_medi, viewGroup, false));
-//            } else if (viewType == TYPE_BAND) {
-//                return new TokenBandHolder(getLayoutInflater().inflate(R.layout.layout_card_band_token, viewGroup, false));
-
+            if (viewType == TYPE_STAKE_NEW) {
+                return new TokenStakingNewHolder(getLayoutInflater().inflate(R.layout.layout_card_staking_new, viewGroup, false));
             }
 
             else if (viewType == TYPE_VESTING) {
                 return new VestingHolder(getLayoutInflater().inflate(R.layout.layout_vesting_schedule, viewGroup, false));
+            } else if (viewType == TYPE_UNBONDING) {
+                return new UnBondingHolder(getLayoutInflater().inflate(R.layout.item_wallet_undelegation, viewGroup, false));
             } else if (viewType == TYPE_HISTORY) {
                 return new HistoryHolder(getLayoutInflater().inflate(R.layout.item_history, viewGroup, false));
             }
@@ -284,123 +210,44 @@ public class StakingTokenGrpcActivity extends BaseActivity implements View.OnCli
 
         @Override
         public void onBindViewHolder(@NonNull BaseHolder holder, int position) {
-            if (getItemViewType(position) == TYPE_ATOM) {
+            if (getItemViewType(position) == TYPE_STAKE_NEW) {
                 holder.onBindTokenHolder(getBaseContext(), mBaseChain, getBaseDao(), WDp.mainDenom(mBaseChain));
-
-            } else if (getItemViewType(position) == TYPE_IRIS) {
-                holder.onBindTokenHolder(getBaseContext(), mBaseChain, getBaseDao(), WDp.mainDenom(mBaseChain));
-
-            } else if (getItemViewType(position) == TYPE_AKASH) {
-                holder.onBindTokenHolder(getBaseContext(), mBaseChain, getBaseDao(), WDp.mainDenom(mBaseChain));
-
-            } else if (getItemViewType(position) == TYPE_SENTINEL) {
-                holder.onBindTokenHolder(getBaseContext(), mBaseChain, getBaseDao(), WDp.mainDenom(mBaseChain));
-
-            } else if (getItemViewType(position) == TYPE_PERSISTENCE) {
-                holder.onBindTokenHolder(getBaseContext(), mBaseChain, getBaseDao(), WDp.mainDenom(mBaseChain));
-
-            } else if (getItemViewType(position) == TYPE_CRYPTO) {
-                holder.onBindTokenHolder(getBaseContext(), mBaseChain, getBaseDao(), WDp.mainDenom(mBaseChain));
-
-            } else if (getItemViewType(position) == TYPE_RIZON) {
-                holder.onBindTokenHolder(getBaseContext(), mBaseChain, getBaseDao(), WDp.mainDenom(mBaseChain));
-
-            } else if (getItemViewType(position) == TYPE_ALTHEA) {
-                holder.onBindTokenHolder(getBaseContext(), mBaseChain, getBaseDao(), WDp.mainDenom(mBaseChain));
-
-            } else if (getItemViewType(position) == TYPE_OSMOSIS) {
-                holder.onBindTokenHolder(getBaseContext(), mBaseChain, getBaseDao(), WDp.mainDenom(mBaseChain));
-
-            } else if (getItemViewType(position) == TYPE_STARNAME) {
-                holder.onBindTokenHolder(getBaseContext(), mBaseChain, getBaseDao(), WDp.mainDenom(mBaseChain));
-
-            } else if (getItemViewType(position) == TYPE_SIF) {
-                holder.onBindTokenHolder(getBaseContext(), mBaseChain, getBaseDao(), WDp.mainDenom(mBaseChain));
-
-            } else if (getItemViewType(position) == TYPE_MEDI) {
-                holder.onBindTokenHolder(getBaseContext(), mBaseChain, getBaseDao(), WDp.mainDenom(mBaseChain));
-
-//            } else if (getItemViewType(position) == TYPE_BAND) {
-//                holder.onBindTokenHolder(getBaseContext(), mBaseChain, getBaseDao(), WDp.mainDenom(mBaseChain));
-
             }
 
             else if (getItemViewType(position) == TYPE_VESTING) {
                 holder.onBindTokenHolder(getBaseContext(), mBaseChain, getBaseDao(), WDp.mainDenom(mBaseChain));
 
+            } else if (getItemViewType(position) == TYPE_UNBONDING) {
+                holder.onBindTokenHolder(getBaseContext(), mBaseChain, getBaseDao(), WDp.mainDenom(mBaseChain));
+
             } else if (getItemViewType(position) == TYPE_HISTORY) {
 
             }
-//            if (getItemViewType(position) == TYPE_HISTORY) {
-//                ResApiTxList.Data tx = null;
-//                if (mApiTxCustomHistory.size() > 0) {
-//
-//                } else if (mApiTxHistory.size() > 0) {
-//                    if (mHasVesting) {
-//                        tx = mApiTxHistory.get(position - 2);
-//                    } else {
-//                        tx = mApiTxHistory.get(position - 1);
-//                    }
-//                    ((HistoryHolder)holder).onBindHistory(StakingTokenDetailActivity.this, tx, mAccount.address);
-//                }
-//
-//            }
         }
 
         @Override
         public int getItemCount() {
             if (mHasVesting) {
-                return 2;
+                if (getBaseDao().mGrpcUndelegations.size() > 0) {
+                    return 3;
+                } else {
+                    return 2;
+                }
+            } else {
+                return 1;
             }
-            return 1;
-//            int cnt = 1;
-//            if (mApiTxCustomHistory != null) {
-//                cnt = cnt + mApiTxCustomHistory.size();
-//            }
-//            if (mApiTxHistory != null) {
-//                cnt = cnt + mApiTxHistory.size();
-//            }
-//            if (mHasVesting) {
-//                cnt = cnt + 1;
-//            }
-//            return cnt;
         }
 
         @Override
         public int getItemViewType(int position) {
             if (position == 0) {
-                if ((mBaseChain.equals(COSMOS_MAIN) || mBaseChain.equals(COSMOS_TEST))) {
-                    return TYPE_ATOM;
-                } else if (mBaseChain.equals(IRIS_MAIN) || mBaseChain.equals(IRIS_TEST)) {
-                    return TYPE_IRIS;
-                } else if (mBaseChain.equals(AKASH_MAIN)) {
-                    return TYPE_AKASH;
-                } else if (mBaseChain.equals(SENTINEL_MAIN)) {
-                    return TYPE_SENTINEL;
-                } else if (mBaseChain.equals(PERSIS_MAIN)) {
-                    return TYPE_PERSISTENCE;
-                } else if (mBaseChain.equals(CRYPTO_MAIN)) {
-                    return TYPE_CRYPTO;
-                } else if (mBaseChain.equals(OSMOSIS_MAIN)) {
-                    return TYPE_OSMOSIS;
-                } else if (mBaseChain.equals(IOV_MAIN)) {
-                    return TYPE_STARNAME;
-                } else if (mBaseChain.equals(RIZON_TEST)) {
-                    return TYPE_RIZON;
-                } else if (mBaseChain.equals(ALTHEA_TEST)) {
-                    return TYPE_ALTHEA;
-                } else if (mBaseChain.equals(SIF_MAIN)) {
-                    return TYPE_SIF;
-                } else if (mBaseChain.equals(MEDI_MAIN) || mBaseChain.equals(MEDI_TEST)) {
-                    return TYPE_MEDI;
-//                } else if (mBaseChain.equals(BAND_MAIN)) {
-//                    return TYPE_BAND;
-                }
-
-                return -1;
+                return TYPE_STAKE_NEW;
 
             } else if (position == 1 && mHasVesting) {
                 return TYPE_VESTING;
+
+            } else if (position == 2 && getBaseDao().mGrpcUndelegations.size() > 0) {
+                return TYPE_UNBONDING;
 
             } else {
                 return TYPE_HISTORY;
---
+++ Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/widget/tokenDetail/TokenStakingNewHolder.java
@@ -0,0 +1,58 @@
+package wannabit.io.cosmostaion.widget.tokenDetail;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+
+import java.math.BigDecimal;
+
+import wannabit.io.cosmostaion.R;
+import wannabit.io.cosmostaion.base.BaseChain;
+import wannabit.io.cosmostaion.base.BaseData;
+import wannabit.io.cosmostaion.utils.WDp;
+import wannabit.io.cosmostaion.widget.BaseHolder;
+
+public class TokenStakingNewHolder extends BaseHolder {
+    private CardView            mCardRoot;
+    private RelativeLayout      mVestingLayer;
+
+    private TextView            mTotalAmount, mAvailableAmount;
+    private TextView            mVestingAmount;
+    private TextView            mDelegatedAmount, mUnbondingAmount, mRewardAmount;
+
+    public TokenStakingNewHolder(@NonNull View itemView) {
+        super(itemView);
+        mCardRoot               = itemView.findViewById(R.id.card_root);
+        mTotalAmount            = itemView.findViewById(R.id.total_amount);
+        mAvailableAmount        = itemView.findViewById(R.id.available_amount);
+        mVestingAmount          = itemView.findViewById(R.id.vesting_amount);
+        mDelegatedAmount        = itemView.findViewById(R.id.delegated_amount);
+        mUnbondingAmount        = itemView.findViewById(R.id.unbonding_amount);
+        mRewardAmount           = itemView.findViewById(R.id.reward_amount);
+
+        mVestingLayer           = itemView.findViewById(R.id.vesting_layer);
+    }
+
+    @Override
+    public void onBindTokenHolder(Context c, BaseChain chain, BaseData baseData, String denom) {
+        final int stakingDivideDecimal = WDp.mainDivideDecimal(chain);
+        final int stakingDisplayDecimal = WDp.mainDisplayDecimal(chain);
+        final BigDecimal totalToken = baseData.getAllMainAsset(denom);
+        mTotalAmount.setText(WDp.getDpAmount2(c, totalToken, stakingDivideDecimal, stakingDisplayDecimal));
+        mAvailableAmount.setText(WDp.getDpAmount2(c, baseData.getAvailable(denom), stakingDivideDecimal, stakingDisplayDecimal));
+        mDelegatedAmount.setText(WDp.getDpAmount2(c, baseData.getDelegationSum(), stakingDivideDecimal, stakingDisplayDecimal));
+        mUnbondingAmount.setText(WDp.getDpAmount2(c, baseData.getUndelegationSum(), stakingDivideDecimal, stakingDisplayDecimal));
+        mRewardAmount.setText(WDp.getDpAmount2(c, baseData.getRewardSum(denom), stakingDivideDecimal, stakingDisplayDecimal));
+
+        final BigDecimal vestingAmount = baseData.getVesting(denom);
+        if (vestingAmount.compareTo(BigDecimal.ZERO) > 0) {
+            mVestingLayer.setVisibility(View.VISIBLE);
+            mVestingAmount.setText(WDp.getDpAmount2(c, vestingAmount, stakingDivideDecimal, stakingDisplayDecimal));
+        }
+        mCardRoot.setCardBackgroundColor(WDp.getChainBgColor(c, chain));
+    }
+}
