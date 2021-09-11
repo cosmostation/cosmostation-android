16640233546f6cecf276a4ff14da5b73d876e9e0
Kwonhyukjoon <victorehko12@naver.com>
2021-09-11 17:24:56 +0900

    Android - Unneccessary task delete

--- Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/network/SifChain.java
+++ Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/network/SifChain.java
@@ -1,37 +1,9 @@
 package wannabit.io.cosmostaion.network;
 
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 import wannabit.io.cosmostaion.model.SifIncentive;
-import wannabit.io.cosmostaion.network.req.ReqBroadCast;
-import wannabit.io.cosmostaion.network.res.ResBroadTx;
-import wannabit.io.cosmostaion.network.res.ResLcdAccountInfo;
-import wannabit.io.cosmostaion.network.res.ResLcdAllRewards;
-import wannabit.io.cosmostaion.network.res.ResLcdBondings;
-import wannabit.io.cosmostaion.network.res.ResLcdInflation;
-import wannabit.io.cosmostaion.network.res.ResLcdProposal;
-import wannabit.io.cosmostaion.network.res.ResLcdProposalTally;
-import wannabit.io.cosmostaion.network.res.ResLcdProposalVoted;
-import wannabit.io.cosmostaion.network.res.ResLcdProposals;
-import wannabit.io.cosmostaion.network.res.ResLcdProposer;
-import wannabit.io.cosmostaion.network.res.ResLcdRedelegate;
-import wannabit.io.cosmostaion.network.res.ResLcdRewardFromVal;
-import wannabit.io.cosmostaion.network.res.ResLcdSingleBonding;
-import wannabit.io.cosmostaion.network.res.ResLcdSingleUnBonding;
-import wannabit.io.cosmostaion.network.res.ResLcdSingleValidator;
-import wannabit.io.cosmostaion.network.res.ResLcdUnBondings;
-import wannabit.io.cosmostaion.network.res.ResLcdValidators;
-import wannabit.io.cosmostaion.network.res.ResLcdWithDrawAddress;
-import wannabit.io.cosmostaion.network.res.ResMintParam;
-import wannabit.io.cosmostaion.network.res.ResMyVote;
-import wannabit.io.cosmostaion.network.res.ResNodeInfo;
-import wannabit.io.cosmostaion.network.res.ResProvisions;
-import wannabit.io.cosmostaion.network.res.ResStakingPool;
-import wannabit.io.cosmostaion.network.res.ResTxInfo;
 
 public interface SifChain {
     //Liquidity and Validator Rewards
