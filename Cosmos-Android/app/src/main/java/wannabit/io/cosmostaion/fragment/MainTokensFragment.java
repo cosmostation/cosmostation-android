f43462f143fe919db6dc6365bff1726e0dd8daa0
Kwonhyukjoon <victorehko12@naver.com>
2021-09-13 15:42:18 +0900

    Android - token detail path fix

--- Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/fragment/MainTokensFragment.java
+++ Cosmos-Android/app/src/main/java/wannabit/io/cosmostaion/fragment/MainTokensFragment.java
@@ -880,10 +880,12 @@ public class MainTokensFragment extends BaseFragment {
                     Intent intent = new Intent(getMainActivity(), StakingTokenGrpcActivity.class);
                     intent.putExtra("denom", coin.denom);
                     startActivity(intent);
-                } else {
+                } else if (mNativeGrpc.get(position).denom.equalsIgnoreCase(TOKEN_ION)) {
                     Intent intent = new Intent(getMainActivity(), NativeTokenGrpcActivity.class);
                     intent.putExtra("denom", coin.denom);
                     startActivity(intent);
+                } else {
+                    return;
                 }
             }
         });
