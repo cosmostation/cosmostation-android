144765a5d53cdc0f741b5ce0e1b10e905862de7f
TOM <85468864+kimheeje12@users.noreply.github.com>
2022-05-16 10:21:53 +0900

    Update CustomAlertDialog.java
    update!

--- app/src/main/java/wannabit/io/cosmostaion/dialog/CustomAlertDialog.java
+++ app/src/main/java/wannabit/io/cosmostaion/dialog/CustomAlertDialog.java
@@ -91,37 +91,7 @@ public class CustomAlertDialog extends AlertDialog {
 
     private static CustomAlertDialog makeTripleButton(Context context, CharSequence title, CharSequence message, CharSequence Button1Title, View.OnClickListener Button1Listener,
                                                       CharSequence Button2Title, View.OnClickListener Button2Listener, CharSequence Button3Title, View.OnClickListener Button3Listener) {
-        CustomAlertDialog dialog = new CustomAlertDialog(context);
-        if (StringUtils.isEmpty(title)) {
-            dialog.titleTextView.setVisibility(View.GONE);
-        } else {
-            dialog.titleTextView.setText(title);
-            dialog.titleTextView.setVisibility(View.VISIBLE);
-        }
-
-        if (StringUtils.isEmpty(message)) {
-            dialog.messageTextView.setVisibility(View.GONE);
-        } else {
-            dialog.messageTextView.setText(message);
-            dialog.messageTextView.setVisibility(View.VISIBLE);
-        }
-
-        dialog.Button1.setText(Button1Title);
-        dialog.Button1.setOnClickListener(view -> {
-            if (Button1Listener != null) {
-                Button1Listener.onClick(view);
-            }
-            dialog.dismiss();
-        });
-
-        dialog.Button2.setText(Button2Title);
-        dialog.Button2.setOnClickListener(view -> {
-            if (Button2Listener != null) {
-                Button2Listener.onClick(view);
-            }
-            dialog.dismiss();
-        });
-
+        CustomAlertDialog dialog = makeDoubleButton(context, title, message, Button1Title, Button1Listener, Button2Title, Button2Listener);
         dialog.Button3.setText(Button3Title);
         dialog.Button3.setVisibility(View.VISIBLE);
         dialog.Button3.setOnClickListener(view -> {
