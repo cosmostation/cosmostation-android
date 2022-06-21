da8afd557a01b4a0e13b2af21f175e84e4a50528
kimheeje <kimheeje12@gmail.com>
2022-06-21 15:10:22 +0900

    [ANDWL-223] onActivityResult deprecated refactoring2

--- app/src/main/java/wannabit/io/cosmostaion/fragment/RewardAddressChangeStep3Fragment.java
+++ app/src/main/java/wannabit/io/cosmostaion/fragment/RewardAddressChangeStep3Fragment.java
@@ -1,7 +1,5 @@
 package wannabit.io.cosmostaion.fragment;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,12 +19,10 @@ import wannabit.io.cosmostaion.utils.WDp;
 
 public class RewardAddressChangeStep3Fragment extends BaseFragment implements View.OnClickListener {
 
-    public final static int CHANGE_REWARD_ADDRESS_CONFIRM_DIALOG = 6016;
-
-    private TextView        mFeeAmount, mFeeType;
-    private TextView        mCurrentAddress, mNewAddress, mMemo;
-    private Button          mBeforeBtn, mConfirmBtn;
-    private int             mDpDecimal = 6;
+    private TextView mFeeAmount, mFeeType;
+    private TextView mCurrentAddress, mNewAddress, mMemo;
+    private Button mBeforeBtn, mConfirmBtn;
+    private int mDpDecimal = 6;
 
     public static RewardAddressChangeStep3Fragment newInstance(Bundle bundle) {
         RewardAddressChangeStep3Fragment fragment = new RewardAddressChangeStep3Fragment();
@@ -42,13 +38,13 @@ public class RewardAddressChangeStep3Fragment extends BaseFragment implements Vi
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_reward_address_change_step3, container, false);
-        mFeeAmount              = rootView.findViewById(R.id.change_reward_address_fees);
-        mFeeType                = rootView.findViewById(R.id.change_reward_address_type);
-        mCurrentAddress         = rootView.findViewById(R.id.current_reward_address);
-        mNewAddress             = rootView.findViewById(R.id.new_reward_address);
-        mMemo                   = rootView.findViewById(R.id.memo);
-        mBeforeBtn              = rootView.findViewById(R.id.btn_before);
-        mConfirmBtn             = rootView.findViewById(R.id.btn_confirm);
+        mFeeAmount = rootView.findViewById(R.id.change_reward_address_fees);
+        mFeeType = rootView.findViewById(R.id.change_reward_address_type);
+        mCurrentAddress = rootView.findViewById(R.id.current_reward_address);
+        mNewAddress = rootView.findViewById(R.id.new_reward_address);
+        mMemo = rootView.findViewById(R.id.memo);
+        mBeforeBtn = rootView.findViewById(R.id.btn_before);
+        mConfirmBtn = rootView.findViewById(R.id.btn_confirm);
 
         mBeforeBtn.setOnClickListener(this);
         mConfirmBtn.setOnClickListener(this);
@@ -68,28 +64,19 @@ public class RewardAddressChangeStep3Fragment extends BaseFragment implements Vi
 
     @Override
     public void onClick(View v) {
-        if(v.equals(mBeforeBtn)) {
+        if (v.equals(mBeforeBtn)) {
             getSActivity().onBeforeStep();
 
         } else if (v.equals(mConfirmBtn)) {
             AlertDialogUtils.showDoubleButtonDialog(getSActivity(), getString(R.string.str_reward_address_change_confirm_title), getString(R.string.str_reward_address_change_confirm_msg),
                     getString(R.string.str_cancel), null,
                     getString(R.string.str_continue), View -> {
-                        Intent resultIntent = new Intent();
-                        onActivityResult(CHANGE_REWARD_ADDRESS_CONFIRM_DIALOG, Activity.RESULT_OK, resultIntent);
+                        getSActivity().onStartRewardAddressChange();
                     }, true);
         }
     }
 
     private RewardAddressChangeActivity getSActivity() {
-        return (RewardAddressChangeActivity)getBaseActivity();
+        return (RewardAddressChangeActivity) getBaseActivity();
     }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == CHANGE_REWARD_ADDRESS_CONFIRM_DIALOG && resultCode == Activity.RESULT_OK) {
-            getSActivity().onStartRewardAddressChange();
-        }
-    }
-
 }
