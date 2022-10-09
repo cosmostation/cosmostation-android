d6a676a286a73b1d264ff470893da9facf4ab588
kimheeje <kimheeje12@gmail.com>
2022-10-09 19:57:31 +0900

    [ANDWL-223] rewardaddress change fragment qr code deprecated update

--- app/src/main/java/wannabit/io/cosmostaion/fragment/txs/common/RewardAddressChangeStep0Fragment.java
+++ app/src/main/java/wannabit/io/cosmostaion/fragment/txs/common/RewardAddressChangeStep0Fragment.java
@@ -1,5 +1,6 @@
 package wannabit.io.cosmostaion.fragment.txs.common;
 
+import android.app.Activity;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
@@ -14,10 +15,14 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 
+import com.google.zxing.client.android.Intents;
 import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
 
 import wannabit.io.cosmostaion.R;
 import wannabit.io.cosmostaion.activities.txs.common.RewardAddressChangeActivity;
@@ -27,10 +32,10 @@ import wannabit.io.cosmostaion.utils.WDp;
 
 public class RewardAddressChangeStep0Fragment extends BaseFragment implements View.OnClickListener {
 
-    private EditText        mAddressInput;
-    private LinearLayout    mBtnQr, mBtnPaste;
-    private TextView        mCurrentAddress;
-    private Button          mCancel, mNextBtn;
+    private EditText mAddressInput;
+    private LinearLayout mBtnQr, mBtnPaste;
+    private TextView mCurrentAddress;
+    private Button mCancel, mNextBtn;
 
     public static RewardAddressChangeStep0Fragment newInstance() {
         return new RewardAddressChangeStep0Fragment();
@@ -43,13 +48,13 @@ public class RewardAddressChangeStep0Fragment extends BaseFragment implements Vi
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView   = inflater.inflate(R.layout.fragment_reward_address_change_step0, container, false);
-        mAddressInput   = rootView.findViewById(R.id.reward_account);
+        View rootView = inflater.inflate(R.layout.fragment_reward_address_change_step0, container, false);
+        mAddressInput = rootView.findViewById(R.id.reward_account);
         mCurrentAddress = rootView.findViewById(R.id.current_address);
-        mBtnQr          = rootView.findViewById(R.id.btn_qr);
-        mBtnPaste       = rootView.findViewById(R.id.btn_paste);
-        mNextBtn        = rootView.findViewById(R.id.btn_next);
-        mCancel         = rootView.findViewById(R.id.btn_cancel);
+        mBtnQr = rootView.findViewById(R.id.btn_qr);
+        mBtnPaste = rootView.findViewById(R.id.btn_paste);
+        mNextBtn = rootView.findViewById(R.id.btn_next);
+        mCancel = rootView.findViewById(R.id.btn_cancel);
 
         mCancel.setOnClickListener(this);
         mNextBtn.setOnClickListener(this);
@@ -83,10 +88,10 @@ public class RewardAddressChangeStep0Fragment extends BaseFragment implements Vi
         } else if (v.equals(mBtnQr)) {
             IntentIntegrator integrator = IntentIntegrator.forSupportFragment(this);
             integrator.setOrientationLocked(true);
-            integrator.initiateScan();
+            rewardAddressChangeQrCode.launch(integrator.createScanIntent());
 
         } else if (v.equals(mBtnPaste)) {
-            ClipboardManager clipboard = (ClipboardManager)getSActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipboardManager clipboard = (ClipboardManager) getSActivity().getSystemService(Context.CLIPBOARD_SERVICE);
             if (clipboard.getPrimaryClip() != null && clipboard.getPrimaryClip().getItemCount() > 0) {
                 String userPaste = clipboard.getPrimaryClip().getItemAt(0).coerceToText(getSActivity()).toString().trim();
                 if (TextUtils.isEmpty(userPaste)) {
@@ -103,19 +108,17 @@ public class RewardAddressChangeStep0Fragment extends BaseFragment implements Vi
     }
 
     private RewardAddressChangeActivity getSActivity() {
-        return (RewardAddressChangeActivity)getBaseActivity();
+        return (RewardAddressChangeActivity) getBaseActivity();
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if (result != null) {
-            if(result.getContents() != null) {
-                mAddressInput.setText(result.getContents().trim());
-                mAddressInput.setSelection(mAddressInput.getText().length());
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
+    private ActivityResultLauncher<Intent> rewardAddressChangeQrCode = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                        mAddressInput.setText(result.getData().getStringExtra(Intents.Scan.RESULT).trim());
+                        mAddressInput.setSelection(mAddressInput.getText().length());
+                    }
+                }
+            });
 }
