c8ada774c0e74f5376e443ab6442af7b4d084b5b
Heeje <85468864+kimheeje12@users.noreply.github.com>
2022-11-21 16:10:25 +0900

    [APP-613] history recyclerview fix (#422)
    * [APP-613] history recyclerview fix
    
    * [APP-613] history issue reset
    
    * [APP-613] code simplify
    
    * [APP-613] history refactoring, unused code delete
    
    * [APP-613] code change

--- app/src/main/java/wannabit/io/cosmostaion/fragment/MainHistoryFragment.java
+++ app/src/main/java/wannabit/io/cosmostaion/fragment/MainHistoryFragment.java
@@ -9,7 +9,6 @@ import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,6 +24,8 @@ import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.google.android.gms.common.util.CollectionUtils;
 
 import java.util.ArrayList;
 
@@ -101,12 +102,7 @@ public class MainHistoryFragment extends BaseFragment implements TaskListener {
         mCardView.setOnClickListener(v -> getMainActivity().onClickQrCopy(mChainConfig, mAccount));
 
         mSwipeRefreshLayout.setColorSchemeColors(ContextCompat.getColor(getMainActivity(), R.color.colorPrimary));
-        mSwipeRefreshLayout.setOnRefreshListener(() -> {
-            mId = 0;
-            mApiNewTxCustomHistory.clear();
-            onFetchHistory();
-            mSwipeRefreshLayout.setRefreshing(false);
-        });
+        mSwipeRefreshLayout.setOnRefreshListener(this::onRefreshTab);
 
         initRecyclerView();
     }
@@ -189,7 +185,7 @@ public class MainHistoryFragment extends BaseFragment implements TaskListener {
 
         if (result.taskType == BaseConstant.TASK_FETCH_BNB_HISTORY) {
             mBnbHistory = (ArrayList<BnbHistory>) result.resultData;
-            if (mBnbHistory != null && mBnbHistory.size() > 0) {
+            if (!CollectionUtils.isEmpty(mBnbHistory)) {
                 mEmptyHistory.setVisibility(View.GONE);
                 mRecyclerView.setVisibility(View.VISIBLE);
                 mHistoryAdapter.notifyDataSetChanged();
@@ -200,7 +196,7 @@ public class MainHistoryFragment extends BaseFragment implements TaskListener {
 
         } else if (result.taskType == BaseConstant.TASK_FETCH_OK_HISTORY) {
             mOkHistory = (ArrayList<ResOkHistory.Data.Hit>) result.resultData;
-            if (result.isSuccess && mOkHistory != null && mOkHistory.size() > 0) {
+            if (!CollectionUtils.isEmpty(mOkHistory)) {
                 mEmptyHistory.setVisibility(View.GONE);
                 mRecyclerView.setVisibility(View.VISIBLE);
                 mHistoryAdapter.notifyDataSetChanged();
@@ -211,7 +207,7 @@ public class MainHistoryFragment extends BaseFragment implements TaskListener {
 
         } else if (result.taskType == BaseConstant.TASK_FETCH_API_ADDRESS_HISTORY) {
             mApiNewTxCustomHistory.addAll((ArrayList<ResApiNewTxListCustom>) result.resultData);
-            if (mApiNewTxCustomHistory != null && mApiNewTxCustomHistory.size() > 0) {
+            if (!CollectionUtils.isEmpty(mApiNewTxCustomHistory)) {
                 mEmptyHistory.setVisibility(View.GONE);
                 mRecyclerView.setVisibility(View.VISIBLE);
                 mHistoryAdapter.notifyDataSetChanged();
