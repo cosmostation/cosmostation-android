d4f1ef464a03f6b572337b8231c365ff144317a1
kimheeje <kimheeje12@gmail.com>
2022-11-23 02:02:41 +0900

    [APP-614] send decimal apply

--- app/src/main/java/wannabit/io/cosmostaion/fragment/txs/common/SendStep1Fragment.java
+++ app/src/main/java/wannabit/io/cosmostaion/fragment/txs/common/SendStep1Fragment.java
@@ -204,12 +204,11 @@ public class SendStep1Fragment extends BaseFragment implements View.OnClickListe
             }
         } else if (v.equals(mAdd01)) {
             BigDecimal existed = BigDecimal.ZERO;
-            BigDecimal test = BigDecimal.valueOf(0.1).divide(new BigDecimal("1"), mDpDecimal, RoundingMode.DOWN);
             String es = String.valueOf(mAmountInput.getText()).trim();
             if (es.length() > 0) {
                 existed = new BigDecimal(es);
             }
-            mAmountInput.setText(existed.add(test).toPlainString());
+            mAmountInput.setText(existed.add(BigDecimal.valueOf(0.1).divide(new BigDecimal("1"), mDpDecimal, RoundingMode.DOWN)).toPlainString());
 
         } else if (v.equals(mAdd1)) {
             BigDecimal existed = BigDecimal.ZERO;
