4e92ba0ae6c3c5554f721228318e7a70f7398bf5
kimheeje <kimheeje12@gmail.com>
2022-11-23 17:39:21 +0900

    [APP-351] unbonding time delete

--- app/src/main/java/wannabit/io/cosmostaion/fragment/txs/ok/OKUnbondingFragment3.java
+++ app/src/main/java/wannabit/io/cosmostaion/fragment/txs/ok/OKUnbondingFragment3.java
@@ -16,11 +16,11 @@ import wannabit.io.cosmostaion.utils.WDp;
 
 public class OKUnbondingFragment3 extends BaseFragment implements View.OnClickListener {
 
-    private TextView        mWithdrawAmount;
-    private TextView        mFeeAmount;
-    private TextView        mMemo, mTime;
-    private Button          mBeforeBtn, mConfirmBtn;
-    private TextView        mWithdrawDenom, mFeeDenom;
+    private TextView mWithdrawAmount;
+    private TextView mFeeAmount;
+    private TextView mMemo;
+    private Button mBeforeBtn, mConfirmBtn;
+    private TextView mWithdrawDenom, mFeeDenom;
 
     public static OKUnbondingFragment3 newInstance() {
         return new OKUnbondingFragment3();
@@ -34,14 +34,13 @@ public class OKUnbondingFragment3 extends BaseFragment implements View.OnClickLi
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_stake_withdraw_3, container, false);
-        mWithdrawAmount     = rootView.findViewById(R.id.withdraw_amount);
-        mWithdrawDenom      = rootView.findViewById(R.id.withdraw_amount_denom);
-        mFeeAmount          = rootView.findViewById(R.id.fees_amount);
-        mFeeDenom           = rootView.findViewById(R.id.fees_denom);
-        mMemo               = rootView.findViewById(R.id.memo);
-        mTime               = rootView.findViewById(R.id.unbonding_time);
-        mBeforeBtn          = rootView.findViewById(R.id.btn_before);
-        mConfirmBtn         = rootView.findViewById(R.id.btn_confirm);
+        mWithdrawAmount = rootView.findViewById(R.id.withdraw_amount);
+        mWithdrawDenom = rootView.findViewById(R.id.withdraw_amount_denom);
+        mFeeAmount = rootView.findViewById(R.id.fees_amount);
+        mFeeDenom = rootView.findViewById(R.id.fees_denom);
+        mMemo = rootView.findViewById(R.id.memo);
+        mBeforeBtn = rootView.findViewById(R.id.btn_before);
+        mConfirmBtn = rootView.findViewById(R.id.btn_confirm);
 
         mBeforeBtn.setOnClickListener(this);
         mConfirmBtn.setOnClickListener(this);
@@ -53,13 +52,12 @@ public class OKUnbondingFragment3 extends BaseFragment implements View.OnClickLi
         WDp.setDpCoin(getSActivity(), getBaseDao(), getSActivity().mChainConfig, getSActivity().mTxFee.amount.get(0), mFeeDenom, mFeeAmount);
         WDp.setDpCoin(getSActivity(), getBaseDao(), getSActivity().mChainConfig, getSActivity().mToWithdrawCoin, mWithdrawDenom, mWithdrawAmount);
 
-        mTime.setText(WDp.getUnbondTime(getContext(), getBaseDao(), getSActivity().mBaseChain));
         mMemo.setText(getSActivity().mTxMemo);
     }
 
     @Override
     public void onClick(View v) {
-        if(v.equals(mBeforeBtn)) {
+        if (v.equals(mBeforeBtn)) {
             getSActivity().onBeforeStep();
 
         } else if (v.equals(mConfirmBtn)) {
@@ -69,6 +67,6 @@ public class OKUnbondingFragment3 extends BaseFragment implements View.OnClickLi
     }
 
     private OKUnbondingActivity getSActivity() {
-        return (OKUnbondingActivity)getBaseActivity();
+        return (OKUnbondingActivity) getBaseActivity();
     }
 }
