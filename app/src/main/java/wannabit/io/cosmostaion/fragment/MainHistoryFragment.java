09ac4d6a2368a8584fc4e54d31266a6b4ea6a92a
kimheeje <kimheeje12@gmail.com>
2022-12-05 11:36:33 +0900

    [APP-639] history limit add

--- app/src/main/java/wannabit/io/cosmostaion/fragment/MainHistoryFragment.java
+++ app/src/main/java/wannabit/io/cosmostaion/fragment/MainHistoryFragment.java
@@ -125,7 +125,7 @@ public class MainHistoryFragment extends BaseFragment implements TaskListener {
                 int lastVisibleItemPosition = ((LinearLayoutManager) recyclerView.getLayoutManager()).findLastCompletelyVisibleItemPosition();
                 int itemTotalCount = recyclerView.getAdapter().getItemCount() - 1;
                 if (lastVisibleItemPosition == itemTotalCount && !mBaseChain.equals(BNB_MAIN) && !mBaseChain.equals(OKEX_MAIN)) {
-                    if (!mApiNewTxCustomHistory.isEmpty()) {
+                    if (!mApiNewTxCustomHistory.isEmpty() && mApiNewTxCustomHistory.size() >= 20) {
                         mId = mApiNewTxCustomHistory.get(mApiNewTxCustomHistory.size() - 1).header.id;
                         onFetchHistory();
                     }
@@ -153,8 +153,8 @@ public class MainHistoryFragment extends BaseFragment implements TaskListener {
         mId = 0;
         mApiNewTxCustomHistory.clear();
         mHistoryAdapter.notifyDataSetChanged();
-        onUpdateView();
         onFetchHistory();
+        onUpdateView();
     }
 
     private void onFetchHistory() {
