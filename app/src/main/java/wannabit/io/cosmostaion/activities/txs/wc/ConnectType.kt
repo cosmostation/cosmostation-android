e0d67dd08f3f09a2c6f3a60ee911697e261e4a74
soaryong-c <91711862+soaryong-c@users.noreply.github.com>
2022-12-15 09:46:10 +0900

    #456 support wc scheme (#460)
    * support wc scheme
    
    * support wc scheme

--- app/src/main/java/wannabit/io/cosmostaion/activities/txs/wc/ConnectType.kt
+++ app/src/main/java/wannabit/io/cosmostaion/activities/txs/wc/ConnectType.kt
@@ -1,7 +1,7 @@
 package wannabit.io.cosmostaion.activities.txs.wc
 
 enum class ConnectType {
-    InternalDapp, ExternalDapp, QRWalletConnect, DeepLinkWalletConnect;
+    InternalDapp, ExternalDapp, QRWalletConnect, DeepLinkWalletConnect, DeepLinkWalletConnectCommon;
 
     fun isDapp(): Boolean {
         return this == InternalDapp || this == ExternalDapp
