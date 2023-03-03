e694d6b06ea3b7ad06e702462b93c72937d07a07
kimheeje <kimheeje12@gmail.com>
2023-03-03 18:20:29 +0900

    [APP-710] WaitDialog convert kotlin

--- app/src/main/java/wannabit/io/cosmostaion/dialog/WaitDialog.kt
+++ app/src/main/java/wannabit/io/cosmostaion/dialog/WaitDialog.kt
@@ -1,28 +1,26 @@
-package wannabit.io.cosmostaion.dialog;
+package wannabit.io.cosmostaion.dialog
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import wannabit.io.cosmostaion.R
 
-import androidx.annotation.Nullable;
-import androidx.fragment.app.DialogFragment;
-
-import wannabit.io.cosmostaion.R;
-
-
-public class WaitDialog extends DialogFragment {
-
-    public static WaitDialog newInstance() {
-        WaitDialog frag = new WaitDialog();
-        return frag;
+class WaitDialog : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        dialog!!.window!!.setBackgroundDrawableResource(R.color.colorTrans)
+        dialog!!.window!!.setDimAmount(0.2f)
+        return layoutInflater.inflate(R.layout.dialog_wait, null)
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        getDialog().getWindow().setBackgroundDrawableResource(R.color.colorTrans);
-        getDialog().getWindow().setDimAmount(0.2f);
-
-        return getLayoutInflater().inflate(R.layout.dialog_wait, null);
+    companion object {
+        fun newInstance(): WaitDialog {
+            return WaitDialog()
+        }
     }
 }
