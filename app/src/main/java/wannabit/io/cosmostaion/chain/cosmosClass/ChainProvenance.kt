994c825e1cb7e771c3886d7f631d19b36041e079
Kwonhyukjoon <hyukjoon@stamper.network>
2024-03-09 01:14:50 +0900

    [APP-888] stakedenom bug fixed

--- app/src/main/java/wannabit/io/cosmostaion/chain/cosmosClass/ChainProvenance.kt
+++ app/src/main/java/wannabit/io/cosmostaion/chain/cosmosClass/ChainProvenance.kt
@@ -14,7 +14,7 @@ class ChainProvenance : CosmosLine() {
     override var logo: Int = R.drawable.chain_provenance
     override var swipeLogo: Int = R.drawable.chain_swipe_provenance
     override var apiName: String = "provenance"
-    override var stakeDenom: String? = "upasg"
+    override var stakeDenom: String? = "nhash"
 
     override var accountKeyType = AccountKeyType(PubKeyType.COSMOS_SECP256K1, "m/44'/505'/0'/0/X")
     override var setParentPath: List<ChildNumber> = ImmutableList.of(
