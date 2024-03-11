bbb86108eb57565d0b7f1b37784018e87c880d9f
Kwonhyukjoon <hyukjoon@stamper.network>
2024-03-11 14:50:39 +0900

    [APP-888] fit qr size & dialog navigation bar color fixed

--- app/src/main/java/wannabit/io/cosmostaion/ui/qr/QrDialog.kt
+++ app/src/main/java/wannabit/io/cosmostaion/ui/qr/QrDialog.kt
@@ -8,7 +8,6 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.Window
 import com.google.zxing.BarcodeFormat
