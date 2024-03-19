e3b6d967ad9fb188d03ad89f7ca5d80c6751bf0e
Kwonhyukjoon <hyukjoon@stamper.network>
2024-03-20 03:27:38 +0900

    [APP-911] delete log

--- app/src/main/java/wannabit/io/cosmostaion/ui/main/setting/SettingBottomAdapter.kt
+++ app/src/main/java/wannabit/io/cosmostaion/ui/main/setting/SettingBottomAdapter.kt
@@ -2,7 +2,6 @@ package wannabit.io.cosmostaion.ui.main.setting
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
