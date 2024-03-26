02487a3e5fe0383dcc996ff4a923e8651476d5de
Kwonhyukjoon <hyukjoon@stamper.network>
2024-03-26 12:07:33 +0900

    [APP-916] Support BaseChain Mainnet

---
+++ app/src/main/java/wannabit/io/cosmostaion/chain/evmClass/ChainBaseEvm.kt
@@ -0,0 +1,36 @@
+package wannabit.io.cosmostaion.chain.evmClass
+
+import android.os.Parcelable
+import com.google.common.collect.ImmutableList
+import kotlinx.parcelize.Parcelize
+import org.bitcoinj.crypto.ChildNumber
+import wannabit.io.cosmostaion.R
+import wannabit.io.cosmostaion.chain.AccountKeyType
+import wannabit.io.cosmostaion.chain.EthereumLine
+import wannabit.io.cosmostaion.chain.PubKeyType
+
+@Parcelize
+class ChainBaseEvm : EthereumLine(), Parcelable {
+
+    override var name: String = "Base"
+    override var tag: String = "base60"
+    override var logo: Int = R.drawable.chain_base
+    override var swipeLogo: Int = R.drawable.chain_swipe_base
+    override var apiName: String = "base"
+
+    override var coinSymbol: String = "ETH"
+    override var coinGeckoId: String = "ethereum"
+    override var coinLogo: Int = R.drawable.token_eth_base
+    override var supportStaking = false
+
+    override var accountKeyType = AccountKeyType(PubKeyType.ETH_KECCAK256, "m/44'/60'/0'/0/X")
+    override var setParentPath: List<ChildNumber> = ImmutableList.of(
+        ChildNumber(44, true), ChildNumber(60, true), ChildNumber.ZERO_HARDENED, ChildNumber.ZERO
+    )
+
+    override var rpcUrl: String = "https://mainnet.base.org"
+
+    override var explorerURL = "https://basescan.org/"
+    override var addressURL = explorerURL + "address/"
+    override var txURL = explorerURL + "tx/"
+}
