14d4049463014437d9a99a73dcaded1797e26ea6
Kwonhyukjoon <hyukjoon@stamper.network>
2024-04-02 15:58:00 +0900

    [APP-932] Change config.json api url

--- app/src/main/java/wannabit/io/cosmostaion/data/api/ChainApi.kt
+++ app/src/main/java/wannabit/io/cosmostaion/data/api/ChainApi.kt
@@ -1,10 +1,9 @@
 package wannabit.io.cosmostaion.data.api
 
-import retrofit2.Response
 import retrofit2.http.GET
 import wannabit.io.cosmostaion.data.model.res.SupportConfig
 
 interface ChainApi {
-    @GET("/cosmostation/chainlist/master/dapp/config.json")
+    @GET("/cosmostation/chainlist/master/wallet_mobiles/dapp/config.json")
     suspend fun supportConfig(): SupportConfig
 }
