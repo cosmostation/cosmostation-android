085e35cf5e776b15f9186ec76ff5188caa99fafe
Kwonhyukjoon <hyukjoon@stamper.network>
2024-07-18 15:25:41 +0900

    [APP] bug fixed with secret

--- app/src/main/java/wannabit/io/cosmostaion/chain/cosmosClass/ChainSecret118.kt
+++ app/src/main/java/wannabit/io/cosmostaion/chain/cosmosClass/ChainSecret118.kt
@@ -16,6 +16,7 @@ class ChainSecret118 : BaseChain(), Parcelable {
     override var tag: String = "secret118"
     override var logo: Int = R.drawable.chain_secret
     override var swipeLogo: Int = R.drawable.chain_swipe_secret
+    override var isDefault: Boolean = false
     override var apiName: String = "secret"
 
     override var accountKeyType = AccountKeyType(PubKeyType.COSMOS_SECP256K1, "m/44'/118'/0'/0/X")
