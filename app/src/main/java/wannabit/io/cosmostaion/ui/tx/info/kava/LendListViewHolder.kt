0dc9dacb8846cc4aa0aa80662817249fe6b8a71e
yongjoo <yongjoo@wannabit.io>
2024-08-02 17:58:03 +0900

    [APP]Kava hard pool image url update

--- app/src/main/java/wannabit/io/cosmostaion/ui/tx/info/kava/LendListViewHolder.kt
+++ app/src/main/java/wannabit/io/cosmostaion/ui/tx/info/kava/LendListViewHolder.kt
@@ -40,7 +40,7 @@ class LendListViewHolder(
             BaseData.assets?.firstOrNull { it.denom == lendMarket?.denom }?.let { asset ->
                 asset.decimals?.let { decimal ->
                     val lendImgDenom = asset.origin_denom
-                    Picasso.get().load(KAVA_LEND_IMG_URL + "lp" + lendImgDenom + ".png").fit()
+                    Picasso.get().load(KAVA_LEND_IMG_URL + "lp" + lendImgDenom?.replace("/",":") + ".png").fit()
                         .into(marketImg)
                     val title = lendMarket?.spotMarketId?.replace(":30", "")?.replace(":720", "")
                     marketType.text = title?.uppercase()
