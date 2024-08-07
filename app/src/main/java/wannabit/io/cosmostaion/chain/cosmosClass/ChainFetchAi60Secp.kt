459aeeae625d61f11676b4db785be2ec6233a475
Kwonhyukjoon <hyukjoon@stamper.network>
2024-08-07 12:23:23 +0900

    [APP-1026] update fetchai name & image

--- app/src/main/java/wannabit/io/cosmostaion/chain/cosmosClass/ChainFetchAi60Secp.kt
+++ app/src/main/java/wannabit/io/cosmostaion/chain/cosmosClass/ChainFetchAi60Secp.kt
@@ -4,19 +4,13 @@ import android.os.Parcelable
 import com.google.common.collect.ImmutableList
 import kotlinx.parcelize.Parcelize
 import org.bitcoinj.crypto.ChildNumber
-import wannabit.io.cosmostaion.R
 import wannabit.io.cosmostaion.chain.AccountKeyType
-import wannabit.io.cosmostaion.chain.BaseChain
-import wannabit.io.cosmostaion.chain.CosmosEndPointType
 import wannabit.io.cosmostaion.chain.PubKeyType
 
 @Parcelize
-class ChainFetchAi60Secp : BaseChain(), Parcelable {
+class ChainFetchAi60Secp : ChainFetchAi(), Parcelable {
 
-    override var name: String = "Fetch.Ai"
     override var tag: String = "fetchai60_Secp"
-    override var logo: Int = R.drawable.chain_fetchai
-    override var swipeLogo: Int = R.drawable.chain_swipe_fetchai
     override var isDefault: Boolean = false
     override var apiName: String = "fetchai"
 
@@ -24,10 +18,4 @@ class ChainFetchAi60Secp : BaseChain(), Parcelable {
     override var setParentPath: List<ChildNumber> = ImmutableList.of(
         ChildNumber(44, true), ChildNumber(60, true), ChildNumber.ZERO_HARDENED, ChildNumber.ZERO
     )
-
-    override var cosmosEndPointType: CosmosEndPointType? = CosmosEndPointType.USE_GRPC
-    override var stakeDenom: String = "afet"
-    override var accountPrefix: String = "fetch"
-    override var grpcHost: String = "grpc-fetchai.cosmostation.io"
-    override var lcdUrl: String = "https://lcd-fetchai.cosmostation.io/"
 }
