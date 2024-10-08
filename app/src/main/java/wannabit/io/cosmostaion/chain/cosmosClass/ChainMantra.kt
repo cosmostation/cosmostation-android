ac778f547bfd6c18d9d78d31ed6d0f682b45a272
Kwonhyukjoon <hyukjoon@stamper.network>
2024-10-08 17:59:18 +0900

    [APP-1057] Support Mantra mainnet

---
+++ app/src/main/java/wannabit/io/cosmostaion/chain/cosmosClass/ChainMantra.kt
@@ -0,0 +1,31 @@
+package wannabit.io.cosmostaion.chain.cosmosClass
+
+import android.os.Parcelable
+import com.google.common.collect.ImmutableList
+import kotlinx.parcelize.Parcelize
+import org.bitcoinj.crypto.ChildNumber
+import wannabit.io.cosmostaion.R
+import wannabit.io.cosmostaion.chain.AccountKeyType
+import wannabit.io.cosmostaion.chain.BaseChain
+import wannabit.io.cosmostaion.chain.CosmosEndPointType
+import wannabit.io.cosmostaion.chain.PubKeyType
+
+@Parcelize
+class ChainMantra : BaseChain(), Parcelable {
+
+    override var name: String = "Mantra"
+    override var tag: String = "mantra118"
+    override var logo: Int = R.drawable.chain_mantra
+    override var apiName: String = "mantra"
+
+    override var accountKeyType = AccountKeyType(PubKeyType.COSMOS_SECP256K1, "m/44'/118'/0'/0/X")
+    override var setParentPath: List<ChildNumber> = ImmutableList.of(
+        ChildNumber(44, true), ChildNumber(118, true), ChildNumber.ZERO_HARDENED, ChildNumber.ZERO
+    )
+
+    override var cosmosEndPointType: CosmosEndPointType? = CosmosEndPointType.USE_GRPC
+    override var stakeDenom: String = "uom"
+    override var accountPrefix: String = "mantra"
+    override var grpcHost: String = "grpc-mantra.cosmostation.io"
+    override var lcdUrl: String = "https://lcd-mantra.cosmostation.io/"
+}
