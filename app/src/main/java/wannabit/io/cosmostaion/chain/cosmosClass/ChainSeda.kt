51f0223e1bef05f9e2155916183588cf3de68cfc
Kwonhyukjoon <hyukjoon@stamper.network>
2024-11-28 16:27:17 +0900

    [APP-1075] init Custom chain

---
+++ app/src/main/java/wannabit/io/cosmostaion/chain/cosmosClass/ChainSeda.kt
@@ -0,0 +1,31 @@
+package wannabit.io.cosmostaion.chain.cosmosClass
+
+import android.os.Parcelable
+import com.google.common.collect.ImmutableList
+import kotlinx.parcelize.Parcelize
+import org.bitcoinj.crypto.ChildNumber
+import wannabit.io.cosmostaion.R
+import wannabit.io.cosmostaion.chain.AccountKeyType
+import wannabit.io.cosmostaion.chain.BaseChain
+import wannabit.io.cosmostaion.chain.CosmosEndPointType
+import wannabit.io.cosmostaion.chain.PubKeyType
+
+@Parcelize
+class ChainSeda : BaseChain(), Parcelable {
+
+    override var name: String = "Seda"
+    override var tag: String = "seda118"
+    override var logo: Int = R.drawable.chain_seda
+    override var apiName: String = "seda"
+
+    override var accountKeyType = AccountKeyType(PubKeyType.COSMOS_SECP256K1, "m/44'/118'/0'/0/X")
+    override var setParentPath: List<ChildNumber> = ImmutableList.of(
+        ChildNumber(44, true), ChildNumber(118, true), ChildNumber.ZERO_HARDENED, ChildNumber.ZERO
+    )
+
+    override var cosmosEndPointType: CosmosEndPointType? = CosmosEndPointType.USE_LCD
+    override var stakeDenom: String = "aseda"
+    override var accountPrefix: String = "seda"
+    override var grpcHost: String = "seda.grpc.kjnodes.com"
+    override var lcdUrl: String = "https://lcd.mainnet.seda.xyz/"
+}
---
+++ app/src/main/java/wannabit/io/cosmostaion/chain/cosmosClass/ChainSge.kt
@@ -0,0 +1,31 @@
+package wannabit.io.cosmostaion.chain.cosmosClass
+
+import android.os.Parcelable
+import com.google.common.collect.ImmutableList
+import kotlinx.parcelize.Parcelize
+import org.bitcoinj.crypto.ChildNumber
+import wannabit.io.cosmostaion.R
+import wannabit.io.cosmostaion.chain.AccountKeyType
+import wannabit.io.cosmostaion.chain.BaseChain
+import wannabit.io.cosmostaion.chain.CosmosEndPointType
+import wannabit.io.cosmostaion.chain.PubKeyType
+
+@Parcelize
+class ChainSge : BaseChain(), Parcelable {
+
+    override var name: String = "Sge"
+    override var tag: String = "sge118"
+    override var logo: Int = R.drawable.chain_sge
+    override var apiName: String = "sge"
+
+    override var accountKeyType = AccountKeyType(PubKeyType.COSMOS_SECP256K1, "m/44'/118'/0'/0/X")
+    override var setParentPath: List<ChildNumber> = ImmutableList.of(
+        ChildNumber(44, true), ChildNumber(118, true), ChildNumber.ZERO_HARDENED, ChildNumber.ZERO
+    )
+
+    override var cosmosEndPointType: CosmosEndPointType? = CosmosEndPointType.USE_LCD
+    override var stakeDenom: String = "usge"
+    override var accountPrefix: String = "sge"
+    override var grpcHost: String = "sge-grpc.stakerhouse.com"
+    override var lcdUrl: String = "https://sge-api.polkachu.com/"
+}
