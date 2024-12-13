0ee1d6932bfedcb4a6a01ac84a9c797c5248bd20
Kwonhyukjoon <hyukjoon@stamper.network>
2024-12-13 17:25:26 +0900

    [APP-1049] init Atomone vote ui

--- app/src/main/java/wannabit/io/cosmostaion/ui/tx/genTx/VoteAdapter.kt
+++ app/src/main/java/wannabit/io/cosmostaion/ui/tx/genTx/VoteAdapter.kt
@@ -4,23 +4,42 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import wannabit.io.cosmostaion.chain.BaseChain
+import wannabit.io.cosmostaion.chain.cosmosClass.ChainAtomone
 import wannabit.io.cosmostaion.data.model.res.CosmosProposal
+import wannabit.io.cosmostaion.databinding.ItemAtomoneVoteBinding
 import wannabit.io.cosmostaion.databinding.ItemVoteBinding
 
 class VoteAdapter(
     private var selectChain: BaseChain,
     private var listener: ClickListener
-): ListAdapter<CosmosProposal, VoteViewHolder>(VoteDiffCallback()) {
+) : ListAdapter<CosmosProposal, RecyclerView.ViewHolder>(VoteDiffCallback()) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VoteViewHolder {
-        val binding = ItemVoteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return VoteViewHolder(parent.context, binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if (selectChain is ChainAtomone) {
+            val binding =
+                ItemAtomoneVoteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return AtomoneVoteViewHolder(parent.context, binding)
+
+        } else {
+            val binding =
+                ItemVoteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return VoteViewHolder(parent.context, binding)
+        }
     }
 
-    override fun onBindViewHolder(holder: VoteViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val proposal = currentList[position]
-        holder.bind(selectChain, proposal, listener)
+        when (holder) {
+            is VoteViewHolder -> {
+                holder.bind(selectChain, proposal, listener)
+            }
+
+            is AtomoneVoteViewHolder -> {
+                holder.bind(selectChain as ChainAtomone, proposal, listener)
+            }
+        }
     }
 
     private class VoteDiffCallback : DiffUtil.ItemCallback<CosmosProposal>() {
