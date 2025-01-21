3eaaa0c9b1da27e5c69e7e4974de6f532e80dba6
Kwonhyukjoon <hyukjoon@stamper.network>
2025-01-21 18:59:37 +0900

    [APP-1075] error fix with bitcoin js

--- app/src/main/java/wannabit/io/cosmostaion/ui/main/dapp/PopUpBitSignFragment.kt
+++ app/src/main/java/wannabit/io/cosmostaion/ui/main/dapp/PopUpBitSignFragment.kt
@@ -175,28 +175,41 @@ class PopUpBitSignFragment(
                             val type = txJsonObject["type"].asString
                             val privateKey = selectedChain?.privateKey?.bytesToHex()
 
-                            if (type == "ecdsa") {
-                                val signMessageFunction = """function signMessageFunction() {
+                            try {
+                                if (type == "ecdsa") {
+                                    val signMessageFunction = """function signMessageFunction() {
                                             const signMessage = signMessageECDSA('${message}', '${privateKey}');
                                             return signMessage;
                                         }""".trimMargin()
-                                BitcoinJs.mergeFunction(signMessageFunction)
-                                updateData = BitcoinJs.executeFunction("signMessageFunction()")
+                                    BitcoinJs.mergeFunction(signMessageFunction)
+                                    updateData = BitcoinJs.executeFunction("signMessageFunction()")
 
-                            } else {
-                                val signMessageFunction = """function signMessageFunction() {
+                                } else {
+                                    val signMessageFunction = """function signMessageFunction() {
                                             const signMessage = signMessageBIP322('${message}', '${privateKey}', '${selectedChain?.mainAddress}');
                                             return signMessage;
                                         }""".trimMargin()
-                                BitcoinJs.mergeFunction(signMessageFunction)
-                                updateData = BitcoinJs.executeFunction("signMessageFunction()")
-                            }
+                                    BitcoinJs.mergeFunction(signMessageFunction)
+                                    updateData = BitcoinJs.executeFunction("signMessageFunction()")
+                                }
 
-                            withContext(Dispatchers.Main) {
-                                binding.apply {
-                                    btnConfirm.isEnabled = true
-                                    loading.visibility = View.GONE
-                                    signData.text = message
+                                withContext(Dispatchers.Main) {
+                                    binding.apply {
+                                        btnConfirm.isEnabled = true
+                                        loading.visibility = View.GONE
+                                        signData.text = message
+                                    }
+                                }
+
+                            } catch (e: Exception) {
+                                withContext(Dispatchers.Main) {
+                                    binding.apply {
+                                        loading.visibility = View.GONE
+                                        btnConfirm.updateButtonView(false)
+                                        signData.text = ""
+                                        noTxTxt.visibility = View.VISIBLE
+                                        noTxTxt.text = getString(R.string.error_js)
+                                    }
                                 }
                             }
                         }
@@ -216,49 +229,69 @@ class PopUpBitSignFragment(
                                 btcBalance = btcBalance.add(uTxo["value"].asLong.toBigDecimal())
                             }
 
-                            val signPsbtFunction = """function signPsbtFunction() {
+                            try {
+                                val signPsbtFunction = """function signPsbtFunction() {
                                             const txHex = signPsbt('${data}', '${privateKey}');
                                             return txHex;
                                         }""".trimMargin()
-                            BitcoinJs.mergeFunction(signPsbtFunction)
-                            updateData = BitcoinJs.executeFunction("signPsbtFunction()")
+                                BitcoinJs.mergeFunction(signPsbtFunction)
+                                updateData = BitcoinJs.executeFunction("signPsbtFunction()")
 
-                            val getInOutPutsFunction = """function getInOutPutsFunction() {
+                                val getInOutPutsFunction = """function getInOutPutsFunction() {
                                             const inputOutput = getInOutPuts('${data}', '${network}');
                                             return inputOutput;
                                         }""".trimMargin()
-                            BitcoinJs.mergeFunction(getInOutPutsFunction)
-                            val messageData =
-                                BitcoinJs.executeFunction("getInOutPutsFunction()").toString()
-                            val messageJsonObject = JsonParser.parseString(messageData).asJsonObject
-                            val outputs = messageJsonObject["outputs"].asJsonArray
-                            var outputBalance = BigDecimal.ZERO
-                            outputs.forEach { output ->
-                                if (output.asJsonObject["address"].asString != selectedChain?.mainAddress) {
-                                    outputBalance =
-                                        outputBalance.add(output.asJsonObject["value"].asString.toBigDecimal())
+                                BitcoinJs.mergeFunction(getInOutPutsFunction)
+                                val messageData =
+                                    BitcoinJs.executeFunction("getInOutPutsFunction()").toString()
+                                val messageJsonObject = JsonParser.parseString(messageData).asJsonObject
+                                val outputs = messageJsonObject["outputs"].asJsonArray
+                                var outputBalance = BigDecimal.ZERO
+                                outputs.forEach { output ->
+                                    if (output.asJsonObject["address"].asString != selectedChain?.mainAddress) {
+                                        outputBalance =
+                                            outputBalance.add(output.asJsonObject["value"].asString.toBigDecimal())
+                                    }
                                 }
-                            }
 
-                            withContext(Dispatchers.Main) {
-                                binding.apply {
-                                    val price = BaseData.getPrice(coinGeckoId)
-                                    val amount =
-                                        bitFee.movePointLeft(8).setScale(8, RoundingMode.UP)
-                                    val value = price.multiply(amount)
+                                withContext(Dispatchers.Main) {
+                                    binding.apply {
+                                        val price = BaseData.getPrice(coinGeckoId)
+                                        val amount =
+                                            bitFee.movePointLeft(8).setScale(8, RoundingMode.UP)
+                                        val value = price.multiply(amount)
 
-                                    btnConfirm.isEnabled = true
-                                    loading.visibility = View.GONE
-                                    signData.text = formatJsonString(messageData)
-                                    feeAmount.text = formatAmount(amount.toPlainString(), 8)
-                                    feeValue.text = formatAssetValue(value)
+                                        loading.visibility = View.GONE
+                                        signData.text = formatJsonString(messageData)
+                                        feeAmount.text = formatAmount(amount.toPlainString(), 8)
+                                        feeValue.text = formatAssetValue(value)
 
-                                    if (bitFee.add(outputBalance) > btcBalance) {
+                                        if (bitFee.add(outputBalance) > btcBalance) {
+                                            btnConfirm.updateButtonView(false)
+                                            noTxTxt.visibility = View.VISIBLE
+                                        } else {
+                                            btnConfirm.updateButtonView(true)
+                                            noTxTxt.visibility = View.GONE
+                                        }
+                                    }
+                                }
+
+                            } catch (e: Exception) {
+                                withContext(Dispatchers.Main) {
+                                    binding.apply {
+                                        val price = BaseData.getPrice(coinGeckoId)
+                                        val amount =
+                                            bitFee.movePointLeft(8).setScale(8, RoundingMode.UP)
+                                        val value = price.multiply(amount)
+
+                                        loading.visibility = View.GONE
+                                        signData.text = ""
+                                        feeAmount.text = formatAmount(amount.toPlainString(), 8)
+                                        feeValue.text = formatAssetValue(value)
+
                                         btnConfirm.updateButtonView(false)
                                         noTxTxt.visibility = View.VISIBLE
-                                    } else {
-                                        btnConfirm.updateButtonView(true)
-                                        noTxTxt.visibility = View.GONE
+                                        noTxTxt.text = getString(R.string.error_js)
                                     }
                                 }
                             }
