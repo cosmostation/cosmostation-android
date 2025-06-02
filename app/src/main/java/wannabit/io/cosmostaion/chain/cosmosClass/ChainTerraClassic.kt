4d0eea77f75fa938149b980f2cf81518a20e68e7
Teck <41478467+niodamian@users.noreply.github.com>
2025-06-02 18:00:11 +0300

    Create ChainTerraClassic.kt

---
+++ app/src/main/java/wannabit/io/cosmostaion/chain/cosmosClass/ChainTerraClassic.kt
@@ -0,0 +1,30 @@
+package wannabit.io.cosmostaion.chain.cosmosClass
+
+import android.os.Parcelable
+import com.google.common.collect.ImmutableList
+import kotlinx.parcelize.Parcelize
+import org.bitcoinj.crypto.ChildNumber
+import wannabit.io.cosmostaion.R
+import wannabit.io.cosmostaion.chain.AccountKeyType
+import wannabit.io.cosmostaion.chain.BaseChain
+import wannabit.io.cosmostaion.chain.CosmosEndPointType
+import wannabit.io.cosmostaion.chain.PubKeyType
+
+@Parcelize
+class ChainTerra : BaseChain(), Parcelable {https://github.com/copilot
+
+    override var name: String = "TerraClassic"
+    override var tag: String = "terra330"
+    override var apiName: String = "terra"
+
+    override var accountKeyType = AccountKeyType(PubKeyType.COSMOS_SECP256K1, "m/44'/330'/0'/0/X")
+    override var setParentPath: List<ChildNumber> = ImmutableList.of(
+        ChildNumber(44, true), ChildNumber(330, true), ChildNumber.ZERO_HARDENED, ChildNumber.ZERO
+    )
+
+    override var cosmosEndPointType: CosmosEndPointType? = CosmosEndPointType.USE_GRPC
+    override var stakeDenom: String = "uluna"
+    override var accountPrefix: String = "terra"
+    override var grpcHost: String = "https://terra-classic-grpc-web.publicnode.com"
+    override var lcdUrl: String = "https://terra-classic-lcd.publicnode.com/"
+}
