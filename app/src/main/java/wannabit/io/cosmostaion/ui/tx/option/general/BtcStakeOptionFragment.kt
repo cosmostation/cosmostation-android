901a344c7ae842793f04a214e12437ad887a65cc
Kwonhyukjoon <hyukjoon@stamper.network>
2025-08-08 16:52:55 +0900

    [APP] btc unstaking error fixed

--- app/src/main/java/wannabit/io/cosmostaion/ui/tx/option/general/BtcStakeOptionFragment.kt
+++ app/src/main/java/wannabit/io/cosmostaion/ui/tx/option/general/BtcStakeOptionFragment.kt
@@ -8,12 +8,16 @@ import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.gson.JsonObject
+import wannabit.io.cosmostaion.R
 import wannabit.io.cosmostaion.chain.BaseChain
 import wannabit.io.cosmostaion.chain.fetcher.FinalityProvider
+import wannabit.io.cosmostaion.chain.majorClass.ChainBitCoin86
+import wannabit.io.cosmostaion.common.makeToastWithData
 import wannabit.io.cosmostaion.common.toHex
 import wannabit.io.cosmostaion.databinding.FragmentBtcStakeOptionBinding
 import wannabit.io.cosmostaion.ui.tx.genTx.major.bit.BtcStakingFragment
 import wannabit.io.cosmostaion.ui.tx.genTx.major.bit.BtcUnStakingFragment
+import java.math.RoundingMode
 
 class BtcStakeOptionFragment(
     private val selectedChain: BaseChain, private val staked: Pair<JsonObject, FinalityProvider>?
@@ -40,9 +44,29 @@ class BtcStakeOptionFragment(
     private fun setUpClickAction() {
         binding.apply {
             stakeLayout.setOnClickListener {
+                val btcBalance = (selectedChain as ChainBitCoin86).btcFetcher?.btcBalances
+                val minStakeBalance =
+                    selectedChain.btcFetcher?.btcParams?.minStakingValueSat
+                        ?: 50000L
+                val dpMinStakeBalance =
+                    minStakeBalance.toBigDecimal().movePointLeft(8).setScale(8, RoundingMode.DOWN)
+                val denom = if (selectedChain.isTestnet) {
+                    "sBTC"
+                } else {
+                    "BTC"
+                }
+
+                if (minStakeBalance.toBigDecimal() >= btcBalance) {
+                    requireActivity().makeToastWithData(
+                        R.string.error_at_least_btc_balance,
+                        "$dpMinStakeBalance $denom"
+                    )
+                    return@setOnClickListener
+                }
                 handleOneClickWithDelay(
                     BtcStakingFragment.newInstance(
-                        selectedChain, staked?.second?.provider?.btcPk?.toByteArray()?.toHex()
+                        selectedChain,
+                        staked?.second?.provider?.btcPk?.toByteArray()?.toHex()
                     )
                 )
             }
