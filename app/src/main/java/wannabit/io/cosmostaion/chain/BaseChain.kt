ae3137e37a1c53d47207a7aefe79da35a768461a
Kwonhyukjoon <hyukjoon@stamper.network>
2025-09-17 17:12:44 +0900

    [APP-1177] ongoing eth dapp

--- app/src/main/java/wannabit/io/cosmostaion/chain/BaseChain.kt
+++ app/src/main/java/wannabit/io/cosmostaion/chain/BaseChain.kt
@@ -110,7 +110,6 @@ import wannabit.io.cosmostaion.chain.cosmosClass.ChainSommelier
 import wannabit.io.cosmostaion.chain.cosmosClass.ChainSource
 import wannabit.io.cosmostaion.chain.cosmosClass.ChainStargaze
 import wannabit.io.cosmostaion.chain.cosmosClass.ChainStride
-import wannabit.io.cosmostaion.chain.cosmosClass.ChainSunrise
 import wannabit.io.cosmostaion.chain.cosmosClass.ChainSynternet
 import wannabit.io.cosmostaion.chain.cosmosClass.ChainTeritori
 import wannabit.io.cosmostaion.chain.cosmosClass.ChainTerra
@@ -906,7 +905,6 @@ fun allChains(): MutableList<BaseChain> {
     chains.add(ChainStratosEvm())
     chains.add(ChainStride())
     chains.add(ChainSui())
-//    chains.add(ChainSunrise())
     chains.add(ChainSynternet())
     chains.add(ChainTenetEvm())
     chains.add(ChainTeritori())
