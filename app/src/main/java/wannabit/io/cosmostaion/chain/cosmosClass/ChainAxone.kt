d627078a46a209f0e5daadb2d30bda99af172380
Kwonhyukjoon <hyukjoon@stamper.network>
2025-12-24 10:35:35 +0900

    [APP-1208] Support Axone Mainnet

---
+++ app/src/main/java/wannabit/io/cosmostaion/chain/cosmosClass/ChainAxone.kt
@@ -0,0 +1,29 @@
+package wannabit.io.cosmostaion.chain.cosmosClass
+
+import android.os.Parcelable
+import com.google.common.collect.ImmutableList
+import kotlinx.parcelize.Parcelize
+import org.bitcoinj.crypto.ChildNumber
+import wannabit.io.cosmostaion.chain.AccountKeyType
+import wannabit.io.cosmostaion.chain.BaseChain
+import wannabit.io.cosmostaion.chain.CosmosEndPointType
+import wannabit.io.cosmostaion.chain.PubKeyType
+
+@Parcelize
+class ChainAxone : BaseChain(), Parcelable {
+
+    override var name: String = "Axone"
+    override var tag: String = "axone118"
+    override var apiName: String = "axone"
+
+    override var accountKeyType = AccountKeyType(PubKeyType.COSMOS_SECP256K1, "m/44'/118'/0'/0/X")
+    override var setParentPath: List<ChildNumber> = ImmutableList.of(
+        ChildNumber(44, true), ChildNumber(118, true), ChildNumber.ZERO_HARDENED, ChildNumber.ZERO
+    )
+
+    override var cosmosEndPointType: CosmosEndPointType? = CosmosEndPointType.USE_LCD
+    override var stakeDenom: String = "uaxone"
+    override var accountPrefix: String = "axone"
+    override var grpcHost: String = "grpc.axone.cumulo.com.es"
+    override var lcdUrl: String = "https://api.axone.citizenweb3.com/"
+}
