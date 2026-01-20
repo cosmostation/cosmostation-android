e71e49881a3b48d061d9134aa4b2a79343827c4b
atorrers <Alejandrotorresndro@gmail.com>
2026-01-20 22:05:07 +0800

    Refactor Lists and Magic Numbers

--- app/src/main/java/wannabit/io/cosmostaion/chain/cosmosClass/ChainAirchains.kt
+++ app/src/main/java/wannabit/io/cosmostaion/chain/cosmosClass/ChainAirchains.kt
@@ -1,7 +1,7 @@
 package wannabit.io.cosmostaion.chain.cosmosClass
 
 import android.os.Parcelable
-import com.google.common.collect.ImmutableList
+
 import kotlinx.parcelize.Parcelize
 import org.bitcoinj.crypto.ChildNumber
 import wannabit.io.cosmostaion.chain.AccountKeyType
@@ -17,7 +17,7 @@ open class ChainAirchains : BaseChain(), Parcelable {
     override var apiName: String = "airchains"
 
     override var accountKeyType = AccountKeyType(PubKeyType.COSMOS_SECP256K1, "m/44'/118'/0'/0/X")
-    override var setParentPath: List<ChildNumber> = ImmutableList.of(
+    override var setParentPath: List<ChildNumber> = listOf(
         ChildNumber(44, true), ChildNumber(118, true), ChildNumber.ZERO_HARDENED, ChildNumber.ZERO
     )
 
--- app/src/main/java/wannabit/io/cosmostaion/chain/cosmosClass/ChainHippocrat.kt
+++ app/src/main/java/wannabit/io/cosmostaion/chain/cosmosClass/ChainHippocrat.kt
@@ -1,7 +1,7 @@
 package wannabit.io.cosmostaion.chain.cosmosClass
 
 import android.os.Parcelable
-import com.google.common.collect.ImmutableList
+
 import kotlinx.parcelize.Parcelize
 import org.bitcoinj.crypto.ChildNumber
 import wannabit.io.cosmostaion.chain.AccountKeyType
@@ -17,7 +17,7 @@ class ChainHippocrat : BaseChain(), Parcelable {
     override var apiName: String = "hippocrat"
 
     override var accountKeyType = AccountKeyType(PubKeyType.COSMOS_SECP256K1, "m/44'/0'/0'/0/X")
-    override var setParentPath: List<ChildNumber> = ImmutableList.of(
+    override var setParentPath: List<ChildNumber> = listOf(
         ChildNumber(44, true), ChildNumber.ZERO_HARDENED, ChildNumber.ZERO_HARDENED, ChildNumber.ZERO
     )
 
