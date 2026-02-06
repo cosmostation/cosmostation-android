7a08c56da84a0ada27a96b9808fd0accbb13e8e0
Kwonhyukjoon <hyukjoon@stamper.network>
2026-02-06 12:45:20 +0900

    [APP-1222] Unlisting Bluzelle Mainnet

--- app/src/main/java/wannabit/io/cosmostaion/chain/BaseChain.kt
+++ app/src/main/java/wannabit/io/cosmostaion/chain/BaseChain.kt
@@ -27,7 +27,6 @@ import wannabit.io.cosmostaion.chain.cosmosClass.ChainBand
 import wannabit.io.cosmostaion.chain.cosmosClass.ChainBeezee
 import wannabit.io.cosmostaion.chain.cosmosClass.ChainBitcanna
 import wannabit.io.cosmostaion.chain.cosmosClass.ChainBitsong
-import wannabit.io.cosmostaion.chain.cosmosClass.ChainBluzelle
 import wannabit.io.cosmostaion.chain.cosmosClass.ChainC4E
 import wannabit.io.cosmostaion.chain.cosmosClass.ChainCarbon
 import wannabit.io.cosmostaion.chain.cosmosClass.ChainCelestia
@@ -824,7 +823,6 @@ fun allChains(): MutableList<BaseChain> {
     chains.add(ChainBitsong())
     chains.add(ChainBinanceSmart())
     chains.add(ChainBlast())
-    chains.add(ChainBluzelle())
     chains.add(ChainCantoEvm())
     chains.add(ChainCarbon())
     chains.add(ChainCelestia())
