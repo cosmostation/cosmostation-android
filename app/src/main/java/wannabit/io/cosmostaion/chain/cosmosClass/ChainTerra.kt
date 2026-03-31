cef8093ac57b30142e8dd9717f0664f919aa55d5
Kwonhyukjoon <hyukjoon@stamper.network>
2026-03-31 19:40:58 +0900

    [APP-1226] endpoint refact for t,u,x,z chains

--- app/src/main/java/wannabit/io/cosmostaion/chain/cosmosClass/ChainTerra.kt
+++ app/src/main/java/wannabit/io/cosmostaion/chain/cosmosClass/ChainTerra.kt
@@ -4,7 +4,6 @@ import android.os.Parcelable
 import com.google.common.collect.ImmutableList
 import kotlinx.parcelize.Parcelize
 import org.bitcoinj.crypto.ChildNumber
-import wannabit.io.cosmostaion.R
 import wannabit.io.cosmostaion.chain.AccountKeyType
 import wannabit.io.cosmostaion.chain.BaseChain
 import wannabit.io.cosmostaion.chain.CosmosEndPointType
@@ -22,9 +21,9 @@ class ChainTerra : BaseChain(), Parcelable {
         ChildNumber(44, true), ChildNumber(330, true), ChildNumber.ZERO_HARDENED, ChildNumber.ZERO
     )
 
-    override var cosmosEndPointType: CosmosEndPointType? = CosmosEndPointType.USE_GRPC
+    override var cosmosEndPointType: CosmosEndPointType? = CosmosEndPointType.USE_LCD
     override var stakeDenom: String = "uluna"
     override var accountPrefix: String = "terra"
-    override var grpcHost: String = "grpc-terra.cosmostation.io"
-    override var lcdUrl: String = "https://lcd-terra.cosmostation.io/"
+    override var grpcHost: String = "erra2.tdrsys.com:2083"
+    override var lcdUrl: String = "https://terra2.tdrsys.com/"
 }
--- app/src/main/java/wannabit/io/cosmostaion/chain/evmClass/ChainZetaEvm.kt
+++ app/src/main/java/wannabit/io/cosmostaion/chain/evmClass/ChainZetaEvm.kt
@@ -4,7 +4,6 @@ import android.os.Parcelable
 import com.google.common.collect.ImmutableList
 import kotlinx.parcelize.Parcelize
 import org.bitcoinj.crypto.ChildNumber
-import wannabit.io.cosmostaion.R
 import wannabit.io.cosmostaion.chain.AccountKeyType
 import wannabit.io.cosmostaion.chain.BaseChain
 import wannabit.io.cosmostaion.chain.CosmosEndPointType
@@ -22,13 +21,13 @@ class ChainZetaEvm : BaseChain(), Parcelable {
         ChildNumber(44, true), ChildNumber(60, true), ChildNumber.ZERO_HARDENED, ChildNumber.ZERO
     )
 
-    override var cosmosEndPointType: CosmosEndPointType? = CosmosEndPointType.USE_GRPC
+    override var cosmosEndPointType: CosmosEndPointType? = CosmosEndPointType.USE_LCD
     override var stakeDenom: String = "azeta"
     override var accountPrefix: String = "zeta"
-    override var grpcHost: String = "grpc-zeta.cosmostation.io"
-    override var lcdUrl: String = "https://lcd-zeta.cosmostation.io/"
+    override var grpcHost: String = ""
+    override var lcdUrl: String = "https://zetachain.blockpi.network/lcd/v1/public/"
 
     override var supportEvm: Boolean = true
     override var coinSymbol: String = "ZETA"
-    override var evmRpcURL: String = "https://rpc-zeta-evm.cosmostation.io"
+    override var evmRpcURL: String = "https://zetachain-mainnet.g.allthatnode.com/archive/evm"
 }
